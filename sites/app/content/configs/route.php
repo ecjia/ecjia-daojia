5687a9f1848e2cd2fec4b2c40784625dfe65e4d9
王正东 <hxroyal@gmail.com>
2017-05-10 17:49:51 +0800

    添加app站点

---
+++ sites/app/content/configs/route.php
@@ -0,0 +1,11 @@
+<?php
+defined('IN_ECJIA') or exit('No permission resources.');
+return array(
+	'default' => array(
+	    'm' => 'intro', 
+	    'c' => 'index', 
+	    'a' => 'init'
+	),
+);
+
+// end
---
+++ sites/app/content/themes/ecjia-app/library/shop_subtitle.lbi.php
@@ -0,0 +1,7 @@
+<?php
+/*
+Name: 副标题
+Description: 副标题
+*/
+?>
+<span>同城上门O2O解决方案</span>
