5a1cbcdd051a93c9425399203c7775ea68b87348
王正东 <hxroyal@gmail.com>
2017-05-10 17:50:49 +0800

    更新首页，加强最低版本判断

--- index.php
+++ index.php
@@ -10,6 +10,12 @@ if (!defined('USE_PLATFORM_MOBILE')) define('USE_PLATFORM_MOBILE', true);
 // 站点根目录
 define('SITE_ROOT', dirname(__FILE__) . DIRECTORY_SEPARATOR);
 
+// 判断PHP最低版本
+if (version_compare(PHP_VERSION, '5.4.0', '<')) {
+    echo 'Current PHP Version: ' . PHP_VERSION . ', Required Version: 5.4.0';
+    exit();
+}
+
 require SITE_ROOT . 'vendor/autoload.php';
 
 require ROYALCMS_PATH . 'bootstrap/start.php';
