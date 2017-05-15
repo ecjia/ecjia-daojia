5100aea31eeaf538118294f5897389ef367ab5dd
王正东 <hxroyal@gmail.com>
2017-05-15 14:33:16 +0800

    更新intro应用的语言包

--- content/apps/intro/languages/zh_CN/package.lang.php
+++ content/apps/intro/languages/zh_CN/package.lang.php
@@ -51,7 +51,9 @@ defined('IN_ECJIA') or exit('No permission resources.');
  */
 return array(
 	'main'		=> 'APP介绍',
-	'main_desc'	=> 'ecjia在PC端展示的App下载页面，主要是App的功能介绍，截图展示，提供二维码下载。'
+	'main_desc'	=> 'ecjia在PC端展示的App下载页面，主要是App的功能介绍，截图展示，提供二维码下载。',
+    'intro'     => 'APP介绍',
+    'intro_desc'    => 'ecjia在PC端展示的App下载页面，主要是App的功能介绍，截图展示，提供二维码下载。'
 );
 
 // end
