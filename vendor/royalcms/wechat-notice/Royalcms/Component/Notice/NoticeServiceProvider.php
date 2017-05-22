10cae133ea5ad940b8f10b8ba321a1a023f4de2d
王正东 <hxroyal@gmail.com>
2017-05-22 16:55:00 +0800

    添加wechat-notice组件库

---
+++ vendor/royalcms/wechat-notice/Royalcms/Component/Notice/NoticeServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace royalcms\Component\WeChat\Notice;
+
+use Royalcms\Component\Support\ServiceProvider;
+
+/**
+ * Class NoticeServiceProvider.
+ */
+class NoticeServiceProvider extends ServiceProvider
+{
+    /**
+     * Registers services on the given container.
+     *
+     * This method should only be used to configure services and parameters.
+     * It should not get services.
+     *
+     * @param Container $wechat A container instance
+     */
+    public function register()
+    {
+        $wechat = $this->royalcms['wechat'];
+        
+        $wechat['notice'] = function ($wechat) {
+            return new Notice($wechat['access_token']);
+        };
+    }
+}
