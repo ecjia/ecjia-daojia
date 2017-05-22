2a62be4363f5b4b096c1c92993f462c7f8cb82b6
王正东 <hxroyal@gmail.com>
2017-05-22 16:54:47 +0800

    添加wechat-qrcode组件库

---
+++ vendor/royalcms/wechat-qrcode/Royalcms/Component/QrCode/QrCodeServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Royalcms\Component\WeChat\QrCode;
+
+use Royalcms\Component\Support\ServiceProvider;
+
+/**
+ * Class QrCodeServiceProvider.
+ */
+class QrCodeServiceProvider extends ServiceProvider
+{
+    /**
+     * Registers services on the given container.
+     *
+     * This method should only be used to configure services and parameters.
+     * It should not get services.
+     *
+     * @param Container $wechat A container instance
+     */
+    public function register()
+    {
+        $wechat = $this->royalcms['wechat'];
+        
+        $wechat['qrcode'] = function ($wechat) {
+            return new QRCode($wechat['access_token']);
+        };
+    }
+}
