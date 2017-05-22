66eda80295917b274a4be1c4089ca809a5095c4c
王正东 <hxroyal@gmail.com>
2017-05-22 16:54:04 +0800

    添加wechat-url组件库

---
+++ vendor/royalcms/wechat-url/Royalcms/Component/Url/UrlServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Royalcms\Component\WeChat\Url;
+
+use Royalcms\Component\Support\ServiceProvider;
+
+/**
+ * Class UrlServiceProvider.
+ */
+class UrlServiceProvider extends ServiceProvider
+{
+    /**
+     * Registers services on the given container.
+     *
+     * This method should only be used to configure services and parameters.
+     * It should not get services.
+     *
+     * @param Container $wechat A container instance
+     */
+    public function register()
+    {
+        $wechat = $this->royalcms['wechat'];
+        
+        $wechat['url'] = function ($wechat) {
+            return new Url($wechat['access_token']);
+        };
+    }
+}
