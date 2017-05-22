16c6a3f78a88df187c4e44559bac1da4db4906df
王正东 <hxroyal@gmail.com>
2017-05-22 16:54:34 +0800

    添加wechat-server组件库

---
+++ vendor/royalcms/wechat-server/Royalcms/Component/Server/BadRequestException.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Royalcms\Component\WeChat\Server;
+
+use Royalcms\Component\WeChat\Core\Exception;
+
+/**
+ * Class BadRequestException.
+ */
+class BadRequestException extends Exception
+{
+}
---
+++ vendor/royalcms/wechat-server/Royalcms/Component/Server/ServerServiceProvider.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Royalcms\Component\WeChat\Server;
+
+use Royalcms\Component\WeChat\Encryption\Encryptor;
+use Royalcms\Component\Support\ServiceProvider;
+
+/**
+ * Class ServerServiceProvider.
+ */
+class ServerServiceProvider extends ServiceProvider
+{
+    /**
+     * Registers services on the given container.
+     *
+     * This method should only be used to configure services and parameters.
+     * It should not get services.
+     *
+     * @param Container $wechat A container instance
+     */
+    public function register()
+    {
+        $wechat = $this->royalcms['wechat'];
+        
+        $wechat['encryptor'] = function ($wechat) {
+            return new Encryptor(
+                $wechat['config']['app_id'],
+                $wechat['config']['token'],
+                $wechat['config']['aes_key']
+            );
+        };
+
+        $wechat['server'] = function ($wechat) {
+            $server = new Guard($wechat['config']['token']);
+
+            $server->debug($wechat['config']['debug']);
+
+            $server->setEncryptor($wechat['encryptor']);
+
+            return $server;
+        };
+    }
+}
