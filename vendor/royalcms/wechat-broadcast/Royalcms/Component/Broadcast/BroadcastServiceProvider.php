1aef115c8409b4746bda1def2f51bc3db1d97674
王正东 <hxroyal@gmail.com>
2017-05-22 16:55:58 +0800

    添加wechat-broadcast组件库

---
+++ vendor/royalcms/wechat-broadcast/Royalcms/Component/Broadcast/BroadcastServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Royalcms\Component\WeChat\Broadcast;
+
+use Royalcms\Component\Support\ServiceProvider;
+
+/**
+ * Class BroadcastServiceProvider.
+ */
+class BroadcastServiceProvider extends ServiceProvider
+{
+    /**
+     * Registers services on the given container.
+     *
+     * This method should only be used to configure services and parameters.
+     * It should not get services.
+     *
+     * @param Container $wechat A container instance
+     */
+    public function register()
+    {
+        $wechat = $this->royalcms['wechat'];
+        
+        $wechat['broadcast'] = function ($wechat) {
+            return new Broadcast($wechat['access_token']);
+        };
+    }
+}
