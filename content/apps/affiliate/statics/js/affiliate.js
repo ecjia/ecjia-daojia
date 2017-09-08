4449e4acba3025e23ce5e6ac4100ced1eba7d5de
王正东 <hxroyal@gmail.com>
2017-09-08 15:29:30 +0800

    更新推荐分成模块

--- content/apps/affiliate/statics/js/affiliate.js
+++ content/apps/affiliate/statics/js/affiliate.js
@@ -47,5 +47,25 @@
         		});
         	});
 		},
+		
+        info : function () {
+            app.affiliate.percent_form();
+        },
+        
+        percent_form: function () {
+            var $form = $("form[name='percent_form']");
+            var option = {
+                submitHandler: function () {
+                    $form.ajaxSubmit({
+                        dataType: "json",
+                        success: function (data) {
+                            ecjia.admin.showmessage(data);
+                        }
+                    });
+                }
+            }
+            var options = $.extend(ecjia.admin.defaultOptions.validate, option);
+            $form.validate(options);
+        },
 	};
 })(ecjia.admin, jQuery);
