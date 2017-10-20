3f840f44eae17b9ae4985498734881dc6aded8b5
王正东 <hxroyal@gmail.com>
2017-10-20 17:25:33 +0800

    更新royalcms框架版本至v4.4.0

--- vendor/royalcms/framework/Royalcms/Component/Foundation/Royalcms.php
+++ vendor/royalcms/framework/Royalcms/Component/Foundation/Royalcms.php
@@ -28,14 +28,14 @@ class Royalcms extends Container implements HttpKernelInterface, TerminableInter
 	 *
 	 * @var string
 	 */
-	const VERSION = '4.3.0';
+	const VERSION = '4.4.0';
 	
 	/**
 	 * The Royalcms framework release.
 	 *
 	 * @var string
 	 */
-	const RELEASE = '2017-08-31';
+	const RELEASE = '2017-10-20';
 	
 	/**
 	 * The Royalcms framework minimum PHP version requirements.
