f85b94c32fcfc770064e0645cf6183cffad9ea1d
王正东 <hxroyal@gmail.com>
2017-10-20 16:58:19 +0800

    更新开店向导应用

--- content/apps/shopguide/classes/hooks/merchant_shopguide.class.php
+++ content/apps/shopguide/classes/hooks/merchant_shopguide.class.php
@@ -50,8 +50,8 @@ class shopguide_merchant_hook {
 	
 	static public function shopguide_merchant_menu_api($menus) {
 	    $menu = array(
-	    	ecjia_merchant::make_admin_menu('divider', '', '', 4)->add_purview('shopguide_setup'),
-	    	ecjia_merchant::make_admin_menu('03_shop_guide', __('店铺向导'), RC_Uri::url('shopguide/merchant/init'), 5)->add_purview('shopguide_setup')->add_icon('fa-paper-plane'),
+	    	ecjia_merchant::make_admin_menu('divider', '', '', 6)->add_purview('shopguide_setup'),
+	    	ecjia_merchant::make_admin_menu('07_shop_guide', __('店铺向导'), RC_Uri::url('shopguide/merchant/init'), 7)->add_purview('shopguide_setup')->add_icon('fa-paper-plane'),
 	    );
 	    $menus->add_submenu($menu);
 	    return $menus;
