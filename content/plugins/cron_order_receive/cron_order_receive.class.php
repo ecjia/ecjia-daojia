f86552d652dace694ff46050665fe5d8881efa5e
王正东 <hxroyal@gmail.com>
2017-10-20 17:04:08 +0800

    更新计划任务插件自动确认收货

--- content/plugins/cron_order_receive/cron_order_receive.class.php
+++ content/plugins/cron_order_receive/cron_order_receive.class.php
@@ -49,9 +49,9 @@
  */
 defined('IN_ECJIA') or exit('No permission resources.');
 
-RC_Loader::load_app_class('cron_abstract', 'cron', false);
+use Ecjia\App\Cron\CronAbstract;
 
-class cron_order_receive extends cron_abstract
+class cron_order_receive extends CronAbstract
 {
     
     /**
