9cc17b583a4a94ab117f048758589da578f174e4
王正东 <hxroyal@gmail.com>
2017-10-20 17:25:16 +0800

    更新ecjia框架版本至v1.23

--- content/system/classes/ecjia.class.php
+++ content/system/classes/ecjia.class.php
@@ -50,8 +50,8 @@
 defined('IN_ECJIA') or exit('No permission resources.');
 
 define('APPNAME', 'ECJIA');
-define('VERSION', '1.22');
-define('RELEASE', '20170901'); 
+define('VERSION', '1.23');
+define('RELEASE', '20171020'); 
 
 class ecjia {
 	
