585dd7a61e5c41f8d0cc5406e362f55f0d80f94e
王正东 <hxroyal@gmail.com>
2017-11-01 13:17:40 +0800

    更新商家配送插件

--- content/plugins/ship_o2o_express/languages/zh_CN/plugin.lang.php
+++ content/plugins/ship_o2o_express/languages/zh_CN/plugin.lang.php
@@ -44,22 +44,18 @@
 //
 //  ---------------------------------------------------------------------------------
 //
+/**
+ * o2o立即送
+ */
 defined('IN_ECJIA') or exit('No permission resources.');
 
-RC_Lang::load_plugin('ship_o2o_express');
 return array(
-    'shipping_code' => 'ship_o2o_express',
-	'insure' 		=> false,		/* 不支持保价 */
-	'cod' 			=> true, 		/* 配送方式是否支持货到付款 */
-	'print_model'	=> 2,			/* 模式编辑器 */
-	'print_bg'		=> '',			/* 打印单背景 */
-	'config_lable'	=> '',			/* 打印快递单标签位置信息 */
-		
-	'forms' => array(
-			array('name' => 'item_fee',     'value' => 10),   /* 单件商品的配送价格 */
-			array('name' => 'base_fee',    	'value' => 5),    /* 1000克以内的价格 */
-			array('name' => 'step_fee',     'value' => 5),    /* 续重每1000克增加的价格 */
-	),
+    'ecjia_express'          	=> 'O2O速递',
+    'ecjia_express_desc'     	=> 'O2O速递',
+    
+    'item_fee' 		=> '单件商品费用：',
+    'base_fee' 		=> '首重费用',
+    'step_fee' 		=> '续重费用',
 );
 
 // end
