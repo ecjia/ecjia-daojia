9bb01b586c9b97880bfe952971a3b7fbba690f13
王正东 <hxroyal@gmail.com>
2017-12-06 18:22:42 +0800

    更新各应用的版本号

--- content/apps/setting/configs/package.cfg.php
+++ content/apps/setting/configs/package.cfg.php
@@ -56,7 +56,7 @@ return array(
 		'description' => 'setting_desc',			/* 描述对应的语言项 */
 		'author'      => 'ECJIA TEAM',			/* 作者 */
 		'website'     => 'http://www.ecjia.com',	/* 网址 */
-		'version'     => '2.1.0',					/* 版本号 */
+		'version'     => '2.10.0',					/* 版本号 */
 		'copyright'   => 'ECJIA Copyright 2016.' 
 );
 
