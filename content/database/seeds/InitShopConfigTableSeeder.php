a18d3dd9566d48516d9b332609dbc53b00acbc8b
王正东 <hxroyal@gmail.com>
2017-12-28 16:58:26 +0800

    修复升级的时候会覆盖更新配置数据

--- content/database/seeds/InitShopConfigTableSeeder.php
+++ content/database/seeds/InitShopConfigTableSeeder.php
@@ -87,7 +87,7 @@ class InitShopConfigTableSeeder extends Seeder
     {
         collect($data)->each(function($item) use ($group) {
         	if (ecjia_config::has($item['code'])) {
-        	   ecjia_config::change($group, $item['code'], $item['value'], $item['options']);
+        	   ecjia_config::change($group, $item['code'], null, $item['options']);
         	} else {
         	    ecjia_config::add($group, $item['code'], $item['value'], $item['options']);
         	}
