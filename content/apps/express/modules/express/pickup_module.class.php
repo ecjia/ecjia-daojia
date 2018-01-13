5cb3f5c42420c9c321120b7401ec92561691812e
王正东 <hxroyal@gmail.com>
2018-01-13 16:05:15 +0800

    更新express应用模块

--- content/apps/express/modules/express/pickup_module.class.php
+++ content/apps/express/modules/express/pickup_module.class.php
@@ -68,7 +68,7 @@ class pickup_module extends api_admin implements api_interface {
     	$express_order         = array();
     	$express_order_db      = RC_Model::model('express/express_order_viewmodel');
     	$where                 = array('staff_id' => $_SESSION['staff_id'], 'eo.delivery_sn' => $delivery_sn);
-    	$field                 = 'eo.*, oi.add_time as order_time, oi.pay_time, oi.order_amount, oi.pay_name, sf.merchants_name, sf.address as merchant_address, sf.longitude as merchant_longitude, sf.latitude as merchant_latitude';
+    	$field                 = 'eo.*, oi.add_time as order_time, oi.pay_time, oi.order_amount, oi.pay_name, sf.merchants_name, sf.district as sf_district, sf.street as sf_street, sf.address as merchant_address, sf.longitude as merchant_longitude, sf.latitude as merchant_latitude';
     	$express_order_info    = $express_order_db->field($field)->join(array('delivery_order', 'order_info', 'store_franchisee'))->where($where)->find();
 		
     	if (empty($express_order_info)) {
@@ -82,6 +82,9 @@ class pickup_module extends api_admin implements api_interface {
     	
     	
     	//消息通知
+    	$express_from_address = ecjia_region::getRegionName($express_order_info['sf_district']).ecjia_region::getRegionName($express_order_info['sf_street']).$express_order_info['merchant_address'];
+    	$express_to_address = ecjia_region::getRegionName($express_order_info['district']).ecjia_region::getRegionName($express_order_info['street']).$express_order_info['address'];
+    	
     	$express_order = array(
     		'express_id'	        => $express_order_info['express_id'],
     		'express_sn'	        => $express_order_info['express_sn'],
@@ -89,12 +92,12 @@ class pickup_module extends api_admin implements api_interface {
     		'label_express_type'	=> $express_order_info['from'] == 'assign' ? '系统派单' : '抢单',
     		'order_sn'		        => $express_order_info['order_sn'],
     		'payment_name'	        => $express_order_info['pay_name'],
-    		'express_from_address'	=> '【'.$express_order_info['merchants_name'].'】'. $express_order_info['merchant_address'],
+    		'express_from_address'	=> '【'.$express_order_info['merchants_name'].'】'. $express_from_address,
     		'express_from_location'	=> array(
     			'longitude' => $express_order_info['merchant_longitude'],
     			'latitude'	=> $express_order_info['merchant_latitude'],
     		),
-    		'express_to_address'	=> $express_order_info['address'],
+    		'express_to_address'	=> $express_to_address,
     		'express_to_location'	=> array(
     			'longitude' => $express_order_info['longitude'],
     			'latitude'	=> $express_order_info['latitude'],
@@ -106,7 +109,7 @@ class pickup_module extends api_admin implements api_interface {
     		'order_time'	=> $express_order_info['order_time'] > 0 ? RC_Time::local_date(ecjia::config('time_format'), $express_order_info['order_time']) : '',
     		'pay_time'		=> empty($express_order_info['pay_time']) ? '' : RC_Time::local_date(ecjia::config('time_format'), $express_order_info['pay_time']),
     		'best_time'		=> $express_order_info['best_time'],
-    		'shipping_fee'	=> $express_order_info['shipping_fee'],
+    		'shipping_fee'	=> $express_order_info['commision'],
     		'order_amount'	=> $express_order_info['order_amount'],
     	);
     	
