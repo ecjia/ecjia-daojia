e45793780b862762a7aa23e33682054f2d2cc039
王正东 <hxroyal@gmail.com>
2018-01-13 16:06:13 +0800

    更新mobile应用模块

--- content/apps/mobile/configs/group_code.php
+++ content/apps/mobile/configs/group_code.php
@@ -49,23 +49,23 @@ defined('IN_ECJIA') or exit('No permission resources.');
 /**
  * 移动应用配置
  */
-	return array(
-			array(
-				'code' => 'basic_info',
-				'name' => '基本信息',
-				),
-			array(
-				'code' => 'app_download_url',
-				'name' => 'app下载地址',
+return array(
+		array(
+			'code' => 'basic_info',
+			'name' => '基本信息',
 			),
-			array(
-				'code' => 'mobile_adsense_set',
-				'name' => '移动广告位设置',
-			),
-			array(
-				'code' => 'app_screenshots',
-				'name' => '应用截图',
-			),
-	);
+		array(
+			'code' => 'app_download_url',
+			'name' => 'app下载地址',
+		),
+		array(
+			'code' => 'mobile_adsense_set',
+			'name' => '移动广告位设置',
+		),
+		array(
+			'code' => 'app_screenshots',
+			'name' => '应用截图',
+		),
+);
 	
 //end
