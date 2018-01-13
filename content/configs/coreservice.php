e71dfad55a20f9c2e7652921c4d1c82c49f66153
王正东 <hxroyal@gmail.com>
2018-01-13 16:20:34 +0800

    加载核心配置服务配置

---
+++ content/configs/coreservice.php
@@ -0,0 +1,20 @@
+<?php
+
+return array(
+   
+    
+    /*
+    |--------------------------------------------------------------------------
+    | Autoloaded Service Providers
+    |--------------------------------------------------------------------------
+    |
+    | The service providers listed here will be automatically loaded on the
+    | request to your application. Feel free to add your own services to
+    | this array to grant expanded functionality to your applications.
+    |
+    */
+    
+    'providers' => array(
+        100 => 'Royalcms\Component\NativeSession\NativeSessionServiceProvider',
+    ),
+);
