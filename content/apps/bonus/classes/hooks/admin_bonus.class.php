7d22b6a071cba31f11361a7c8a4f09b12879c71e
东方皓 <hxroyal@gmail.com>
2018-02-05 15:46:04 +0800

    更新bonus应用模块

--- content/apps/bonus/classes/hooks/admin_bonus.class.php
+++ content/apps/bonus/classes/hooks/admin_bonus.class.php
@@ -48,7 +48,7 @@ defined('IN_ECJIA') or exit('No permission resources.');
 
 class bonus_admin_plugin {
 	
-	static public function bonus_admin_menu_api($menus) {
+	public static function bonus_admin_menu_api($menus) {
 	    $menu = ecjia_admin::make_admin_menu('02_bonustype_list', RC_Lang::get('bonus::bonus.bonus_manage'), RC_Uri::url('bonus/admin/init'), 2)->add_purview('bonus_type_manage');
 	    $menus->add_submenu($menu);
 	    return $menus;
--- content/apps/bonus/classes/hooks/merchant_bonus.class.php
+++ content/apps/bonus/classes/hooks/merchant_bonus.class.php
@@ -48,7 +48,7 @@ defined('IN_ECJIA') or exit('No permission resources.');
 
 class bonus_merchant_hook {
 	
-	static public function bonus_merchant_menu_api($menus) {
+	public static function bonus_merchant_menu_api($menus) {
 	    $menu = ecjia_merchant::make_admin_menu('02_bonustype_list', __('红包类型'), RC_Uri::url('bonus/merchant/init'), 2)->add_purview('bonus_type_manage')->add_icon('fa-table');
 	    $menus->add_submenu($menu);
 	    return $menus;
