4c6b06343c9d73acc860da04bf9eb9d87445c60b
东方皓 <hxroyal@gmail.com>
2018-03-12 15:21:34 +0800

    更新ecjia系统版本号为v1.28

--- content/system/classes/ecjia.class.php
+++ content/system/classes/ecjia.class.php
@@ -50,8 +50,8 @@
 defined('IN_ECJIA') or exit('No permission resources.');
 
 define('APPNAME', 'ECJIA');
-define('VERSION', '1.27');
-define('RELEASE', '20180202'); 
+define('VERSION', '1.28');
+define('RELEASE', '20180312'); 
 
 class ecjia {
 	
