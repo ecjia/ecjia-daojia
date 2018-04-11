3941932b20cc11b7134800b163b5005faa25dc70
东方皓 <hxroyal@gmail.com>
2018-04-11 10:27:55 +0800

    更新框架编译缓存类

--- bootstrap/compiled.php
+++ bootstrap/compiled.php
@@ -422,8 +422,8 @@ use Symfony\Component\HttpFoundation\Response as SymfonyResponse;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 class Royalcms extends Container implements HttpKernelInterface, TerminableInterface, ResponsePreparerInterface
 {
-    const VERSION = '4.9.0';
-    const RELEASE = '2018-03-12';
+    const VERSION = '4.10.0';
+    const RELEASE = '2018-04-11';
     const PHP_REQUIRED = '5.4.0';
     protected $booted = false;
     protected $bootingCallbacks = array();
