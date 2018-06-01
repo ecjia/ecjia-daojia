e856c41ff80b5d1402843315139406640f2fd0ab
东方皓 <hxroyal@gmail.com>
2018-06-01 15:44:37 +0800

    更新所有更新过APP的版本号至1.17.0

--- content/apps/user/configs/package.cfg.php
+++ content/apps/user/configs/package.cfg.php
@@ -56,7 +56,7 @@ return array(
 	'description' 	=> 'user_desc',				/* 描述对应的语言项 */
 	'author' 		=> 'ECJIA TEAM',			/* 作者 */
 	'website' 		=> 'http://www.ecjia.com',	/* 网址 */
-	'version' 		=> '1.16.0',					/* 版本号 */
+	'version' 		=> '1.17.0',					/* 版本号 */
 	'copyright' 	=> 'ECJIA Copyright 2014.',
 );
 
