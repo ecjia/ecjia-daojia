9b78441457f201323f4927f65caafc9561631c4e
东方皓 <hxroyal@gmail.com>
2018-06-11 15:31:10 +0800

    更新ecjia系统框架

---
+++ content/system/classes/Frameworks/Ecjia.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Ecjia\System\Frameworks;
+
+
+class Ecjia
+{
+    
+}
---
+++ content/system/classes/Frameworks/Model/Model.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Ecjia\System\Frameworks\Model;
+
+use Royalcms\Component\Rememberable\Rememberable;
+use Royalcms\Component\Database\Eloquent\Model as Eloquent;
+
+abstract class Model extends Eloquent
+{
+    use Rememberable;
+}
