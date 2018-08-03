47f84d2c7c87a27cf6d1c43d16586d78fa338fa8
王正东 <hxroyal@gmail.com>
2018-08-03 13:44:53 +0800

    新增Pipeline组件

---
+++ vendor/royalcms/framework/Royalcms/Component/Pipeline/Contracts/Hub.php
@@ -0,0 +1,16 @@
+<?php 
+
+namespace Royalcms\Component\Pipeline\Contracts;
+
+interface Hub {
+
+	/**
+	 * Send an object through one of the available pipelines.
+	 *
+	 * @param  mixed  $object
+	 * @param  string|null  $pipeline
+	 * @return mixed
+	 */
+	public function pipe($object, $pipeline = null);
+
+}
---
+++ vendor/royalcms/framework/Royalcms/Component/Pipeline/Hub.php
@@ -0,0 +1,75 @@
+<?php 
+
+namespace Royalcms\Component\Pipeline;
+
+use Closure;
+use Royalcms\Component\Container\Contracts\Container;
+use Royalcms\Component\Pipeline\Contracts\Hub as HubContract;
+
+class Hub implements HubContract {
+
+	/**
+	 * The container implementation.
+	 *
+	 * @var \Royalcms\Component\Container\Contracts\Container
+	 */
+	protected $container;
+
+	/**
+	 * All of the available pipelines.
+	 *
+	 * @var array
+	 */
+	protected $pipelines = [];
+
+	/**
+	 * Create a new Depot instance.
+	 *
+	 * @param  \Royalcms\Component\Container\Contracts\Container  $container
+	 * @return void
+	 */
+	public function __construct(Container $container)
+	{
+		$this->container = $container;
+	}
+
+	/**
+	 * Define the default named pipeline.
+	 *
+	 * @param  \Closure  $callback
+	 * @return void
+	 */
+	public function defaults(Closure $callback)
+	{
+		return $this->pipeline('default', $callback);
+	}
+
+	/**
+	 * Define a new named pipeline.
+	 *
+	 * @param  string  $name
+	 * @param  \Closure  $callback
+	 * @return void
+	 */
+	public function pipeline($name, Closure $callback)
+	{
+		$this->pipelines[$name] = $callback;
+	}
+
+	/**
+	 * Send an object through one of the available pipelines.
+	 *
+	 * @param  mixed  $object
+	 * @param  string|null  $pipeline
+	 * @return mixed
+	 */
+	public function pipe($object, $pipeline = null)
+	{
+		$pipeline = $pipeline ?: 'default';
+
+		return call_user_func(
+			$this->pipelines[$pipeline], new Pipeline($this->container), $object
+		);
+	}
+
+}
