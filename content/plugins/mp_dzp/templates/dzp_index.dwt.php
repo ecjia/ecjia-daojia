6b38272805484307bbbb57549dba7beac42fea10
王正东 <hxroyal@gmail.com>
2018-08-03 15:18:12 +0800

    更新微信大转盘插件

--- content/plugins/mp_dzp/templates/dzp_index.dwt.php
+++ content/plugins/mp_dzp/templates/dzp_index.dwt.php
@@ -29,7 +29,7 @@
     <div class="info-box">
         <div class="info-box-inner">
             <h4>剩余次数</h4>
-            <div>您当前还剩余<span style="font-size: 16px;"> {$prize_num} </span>次抽奖机会</div>
+            <div>您当前还剩余<span style="font-size: 16px;"> {if $prize_num lt 0} 0 {else} {$prize_num} {/if}  </span>次抽奖机会</div>
         </div>
     </div>
     <div class="info-box">
@@ -79,10 +79,9 @@
 //             return false;
 //         }
         var dot_round = 0;
-        var lucky_span = $(" span");
+        var lucky_span = $(".lucky span");
         var lucky_p = LUCKY_POS[lucky_span.length];
         lucky_span.each(function(idx, item){
-            console.log(idx);
             item = $(item);
             item.addClass('item' + lucky_p[idx] + ' z' + item.text().length);
             item.rotate(LUCKY_ROTATE[lucky_p[idx]]);
@@ -90,6 +89,7 @@
         var NOL_TXTs = ['再接再厉', '不要灰心', '没有抽中', '谢谢参与', '祝您好运', '不要灰心', '就差一点'];
         for (var i = 1; i <= 12; i++){
             if ($('.lucky .item' + i).length == 0){
+                console.log(LUCKY_ROTATE);
                 var item = $('<span class="item' + i + ' nol z4">' + NOL_TXTs[i > 6 ? 12 - i : i] + '</span>').appendTo('.lucky');
                 item.rotate(LUCKY_ROTATE[i]);
             }
@@ -101,9 +101,9 @@
             $.getJSON('{$form_action}', '', function(data){
                 //中奖
                 if(data.status == 1){
-                    var b = $(".lucky span[data-level='"+data.level+"']").index();
+                    var b = $(".lucky span[data-level='"+data.msg+"']").index();
                     var a = lucky_l[b];
-                    var msg = '恭喜中了' + $(".lucky span[data-level='"+data.level+"']").text();
+                    var msg = '恭喜中了' + $(".lucky span[data-level='"+data.msg+"']").text();
                     $(".point-btn").hide();
                     $(".point-arrow").rotate({ 
                         duration:3000, //转动时间 
@@ -157,7 +157,7 @@
             $('.dots').rotate(dot_round);
         }, 800);
 
-    })
+    });
 
     var POINT_LEVEL = { 3: [30, 150, 270], 4: [30, 90, 210, 270], 5:[30, 90, 150, 210, 270], 6:[30, 90, 150, 210, 270, 330], 7:[30, 90, 150, 210, 270, 330, 0], 8:[30, 90, 150, 210, 270, 330, 0, 180],9:[30, 90, 150, 210, 270, 330, 0, 180, 120] };
     var LUCKY_POS = { 3:[1,5,9], 4:[1,3,7,9], 5:[1,3,5,7,9], 6:[1,3,5,7,9,11], 7:[1,3,5,7,9,11,12], 8:[1,3,5,7,9,11,12,6], 9:[1,3,5,7,9,11,12,6,4] };
