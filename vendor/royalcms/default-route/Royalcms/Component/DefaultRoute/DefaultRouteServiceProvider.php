fa2423d516be502424d15fed9e3a8e8eba0a45c7
王正东 <hxroyal@gmail.com>
2018-08-16 18:00:03 +0800

    更新royalcms/default-route组件

--- vendor/royalcms/default-route/Royalcms/Component/DefaultRoute/DefaultRouteServiceProvider.php
+++ vendor/royalcms/default-route/Royalcms/Component/DefaultRoute/DefaultRouteServiceProvider.php
@@ -8,16 +8,25 @@ class DefaultRouteServiceProvider extends ServiceProvider
 {
     public function boot()
     {
-        $router = royalcms('router');
+        //$router = royalcms('router');
         
-        $router->get('/{action?}', '\Royalcms\Component\DefaultRoute\DefaultRouteController@runAction');
-        $router->any('/{controller}/{action?}', '\Royalcms\Component\DefaultRoute\DefaultRouteController@runControllerAction');
-        $router->any('/{module}/{controller}/{action?}', '\Royalcms\Component\DefaultRoute\DefaultRouteController@runModuleControllerAction');
+        //$router->get('/{action?}', '\Royalcms\Component\DefaultRoute\DefaultRouteController@runAction');
+        //$router->any('/{controller}/{action?}', '\Royalcms\Component\DefaultRoute\DefaultRouteController@runControllerAction');
+        //$router->any('/{module}/{controller}/{action?}', '\Royalcms\Component\DefaultRoute\DefaultRouteController@runModuleControllerAction');
     }
 
     public function register()
     {
-        
+        $this->royalcms['default-router'] = $this->royalcms->share(function($royalcms)
+        {
+            $route = new HttpQueryRoute();
+
+            define('ROUTE_M', $route->getModule());
+            define('ROUTE_C', $route->getController());
+            define('ROUTE_A', $route->getAction());
+
+            return $route;
+        });
     }
     
 }
