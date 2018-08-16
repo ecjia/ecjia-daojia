3886a0b9c47fb72eefcd34f3af117dfd9032034f
王正东 <hxroyal@gmail.com>
2018-08-16 17:25:28 +0800

    更新bonus应用模块

---
+++ content/apps/bonus/modules/bonus/bind_module.class.php
@@ -0,0 +1,42 @@
+<?php
+defined('IN_ECJIA') or exit('No permission resources.');
+/**
+ * 兑换线下红包绑定
+ * @author zrl
+ *
+ */
+class bind_module extends api_front implements api_interface
+{
+    public function handleRequest(\Royalcms\Component\HttpKernel\Request $request)
+    {	
+		$this->authSession();
+		if ($_SESSION['user_id'] <= 0 ) {
+			return new ecjia_error(100, 'Invalid session');
+		}
+		$bonus_sn = $this->requestData('bonus_sn', '0');
+		if (empty($bonus_sn)) {
+			return new ecjia_error('invalid_parameter', RC_Lang::get('system::system.invalid_parameter'));
+		}
+		
+		$time = RC_Time::gmtime();
+    	$db_bonus_view = RC_DB::table('bonus_type as bt')->leftJoin('user_bonus as ub', RC_DB::raw('bt.type_id'), '=', RC_DB::raw('ub.bonus_type_id'));
+    	
+		$bonus_info = $db_bonus_view
+		->where(RC_DB::raw('ub.bonus_sn'), $bonus_sn)
+		->where(RC_DB::raw('ub.user_id'), 0)
+		->where(RC_DB::raw('bt.use_end_date'), '>', $time)
+		->selectRaw('bt.use_start_date, bt.use_end_date, bt.min_goods_amount')
+		->first();
+		
+		if (empty($bonus_info)) {
+			return new ecjia_error('bonus_error', '红包信息有误！');
+		}
+		
+		RC_DB::table('user_bonus')->where('bonus_sn', $bonus_sn)->where('user_id', 0)->where('order_id', 0)->update(array('user_id' => $_SESSION['user_id']));
+		
+		return array();
+ 		
+	}
+}
+
+// end
---
+++ content/apps/bonus/modules/bonus/validate_module.class.php
@@ -0,0 +1,48 @@
+<?php
+defined('IN_ECJIA') or exit('No permission resources.');
+/**
+ * 线下红包兑换验证
+ * @author zrl
+ *
+ */
+class validate_module extends api_front implements api_interface
+{
+    public function handleRequest(\Royalcms\Component\HttpKernel\Request $request)
+    {
+    	$this->authSession();
+    	$bonus_sn = $this->requestData('bonus_sn');
+		
+    	if (empty($bonus_sn)) {
+    		return new ecjia_error('invalid_parameter', RC_Lang::get('system::system.invalid_parameter'));
+    	}
+    	
+		$time = RC_Time::gmtime();
+    	$db_bonus_view = RC_DB::table('bonus_type as bt')->leftJoin('user_bonus as ub', RC_DB::raw('bt.type_id'), '=', RC_DB::raw('ub.bonus_type_id'));
+    	$bonus_info = $db_bonus_view
+    						->where(RC_DB::raw('ub.bonus_sn'), $bonus_sn)
+    						->where(RC_DB::raw('ub.user_id'), 0)
+    						->where(RC_DB::raw('bt.use_end_date'), '>', $time)
+    						->selectRaw('bt.type_id, bt.type_name, bt.type_money, ub.bonus_id, bt.use_start_date, bt.use_end_date, bt.min_goods_amount')
+    						->first();
+    	
+		if (empty($bonus_info)) {
+			return new ecjia_error('bonus_error', '红包信息有误！');
+		}
+		$data = array(
+			'bonus_id'					=> $bonus_info['bonus_id'],
+			'bonus_name'				=> $bonus_info['type_name'],
+			'bonus_amount'				=> $bonus_info['type_money'],
+			'formatted_bonus_amount' 	=> price_format($bonus_info['type_money']),
+			'request_amount'			=> $bonus_info['min_goods_amount'],
+			'formatted_request_amount' 	=> price_format($bonus_info['min_goods_amount']),
+			'label_request_amount' 		=> '购物满'.price_format($bonus_info['min_goods_amount']).'才可以使用此红包',
+			'start_date'				=> $bonus_info['use_start_date'],
+			'end_date'					=> $bonus_info['use_end_date'],
+			'formatted_start_date'   	=> RC_Time::local_date(ecjia::config('date_format'), $bonus_info['use_start_date']),
+			'formatted_end_date'     	=> RC_Time::local_date(ecjia::config('date_format'), $bonus_info['use_end_date']),
+		);
+		return $data; 
+	}
+}
+
+// end
