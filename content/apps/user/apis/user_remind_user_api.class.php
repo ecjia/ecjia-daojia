2440e9062f6f25c5ca8e077bddf6a4777d2d0285
王正东 <hxroyal@gmail.com>
2018-09-04 17:41:47 +0800

    更新会员应用模块

--- content/apps/user/apis/user_remind_user_api.class.php
+++ content/apps/user/apis/user_remind_user_api.class.php
@@ -53,15 +53,12 @@ defined('IN_ECJIA') or exit('No permission resources.');
 class user_remind_user_api extends Component_Event_Api {
 	
 	public function call(&$options) {
-		
-		$db_user = RC_Model::model('user/users_model');
-		if (empty($_SESSION['last_check'])) {
-			$_SESSION['last_check'] = RC_Time::gmtime();
-			return array('new_user_count' => 0);
-		}
-		
-		$new_user = $db_user->where(array('reg_time' => array('egt' => $_SESSION['last_check'])))->count();
-		
+		$m = RC_Time::local_date('m');
+        $d = RC_Time::local_date('d');
+        $y = RC_Time::local_date('y');
+		$today_start_date = RC_Time::local_mktime(0, 0, 0, $m, $d, $y);
+
+		$new_user = RC_DB::table('users')->where('reg_time', '>=', $today_start_date)->count();
 		
 		if ($new_user > 0 ) {
 			return array('new_user_count' => $new_user);
