273249bca41fe1452d8bab3752a76cf6735ca297
王正东 <hxroyal@gmail.com>
2018-09-04 16:00:11 +0800

    更新royalcms/events组件

--- vendor/royalcms/framework/Royalcms/Component/Events/EventServiceProvider.php
+++ vendor/royalcms/framework/Royalcms/Component/Events/EventServiceProvider.php
@@ -1,23 +1,22 @@
-<?php namespace Royalcms\Component\Events;
+<?php
+
+namespace Royalcms\Component\Events;
 
 use Royalcms\Component\Support\ServiceProvider;
 
-class EventServiceProvider extends ServiceProvider {
-
-	/**
-	 * Register the service provider.
-	 *
-	 * @return void
-	 */
-	public function register()
-	{
-		$this->royalcms->singleton('events', function($royalcms)
-		{
-			return with(new Dispatcher($royalcms))->setQueueResolver(function() use ($royalcms)
-			{
-				return $royalcms->make('Royalcms\Component\Contracts\Queue\Queue');
-			});
-		});
-	}
-
+class EventServiceProvider extends ServiceProvider
+{
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->royalcms->singleton('events', function ($royalcms) {
+            return (new Dispatcher($royalcms))->setQueueResolver(function () use ($royalcms) {
+                return $royalcms->make('Royalcms\Component\Contracts\Queue\Factory');
+            });
+        });
+    }
 }
