92061d6256dae9e47d23cde1997229d605543577
王正东 <hxroyal@gmail.com>
2018-09-04 16:02:30 +0800

    更新royalcms/pipeline组件

--- vendor/royalcms/framework/Royalcms/Component/Pipeline/Hub.php
+++ vendor/royalcms/framework/Royalcms/Component/Pipeline/Hub.php
@@ -1,75 +1,74 @@
-<?php 
+<?php
 
 namespace Royalcms\Component\Pipeline;
 
 use Closure;
-use Royalcms\Component\Container\Contracts\Container;
-use Royalcms\Component\Pipeline\Contracts\Hub as HubContract;
+use Royalcms\Component\Contracts\Container\Container;
+use Royalcms\Component\Contracts\Pipeline\Hub as HubContract;
 
-class Hub implements HubContract {
+class Hub implements HubContract
+{
+    /**
+     * The container implementation.
+     *
+     * @var \Royalcms\Component\Contracts\Container\Container
+     */
+    protected $container;
 
-	/**
-	 * The container implementation.
-	 *
-	 * @var \Royalcms\Component\Container\Contracts\Container
-	 */
-	protected $container;
+    /**
+     * All of the available pipelines.
+     *
+     * @var array
+     */
+    protected $pipelines = [];
 
-	/**
-	 * All of the available pipelines.
-	 *
-	 * @var array
-	 */
-	protected $pipelines = [];
+    /**
+     * Create a new Depot instance.
+     *
+     * @param  \Royalcms\Component\Contracts\Container\Container  $container
+     * @return void
+     */
+    public function __construct(Container $container)
+    {
+        $this->container = $container;
+    }
 
-	/**
-	 * Create a new Depot instance.
-	 *
-	 * @param  \Royalcms\Component\Container\Contracts\Container  $container
-	 * @return void
-	 */
-	public function __construct(Container $container)
-	{
-		$this->container = $container;
-	}
+    /**
+     * Define the default named pipeline.
+     *
+     * @param  \Closure  $callback
+     * @return void
+     */
+    public function defaults(Closure $callback)
+    {
+        return $this->pipeline('default', $callback);
+    }
 
-	/**
-	 * Define the default named pipeline.
-	 *
-	 * @param  \Closure  $callback
-	 * @return void
-	 */
-	public function defaults(Closure $callback)
-	{
-		return $this->pipeline('default', $callback);
-	}
+    /**
+     * Define a new named pipeline.
+     *
+     * @param  string  $name
+     * @param  \Closure  $callback
+     * @return void
+     */
+    public function pipeline($name, Closure $callback)
+    {
+        $this->pipelines[$name] = $callback;
+    }
 
-	/**
-	 * Define a new named pipeline.
-	 *
-	 * @param  string  $name
-	 * @param  \Closure  $callback
-	 * @return void
-	 */
-	public function pipeline($name, Closure $callback)
-	{
-		$this->pipelines[$name] = $callback;
-	}
+    /**
+     * Send an object through one of the available pipelines.
+     *
+     * @param  mixed  $object
+     * @param  string|null  $pipeline
+     * @return mixed
+     */
+    public function pipe($object, $pipeline = null)
+    {
+        $pipeline = $pipeline ?: 'default';
 
-	/**
-	 * Send an object through one of the available pipelines.
-	 *
-	 * @param  mixed  $object
-	 * @param  string|null  $pipeline
-	 * @return mixed
-	 */
-	public function pipe($object, $pipeline = null)
-	{
-		$pipeline = $pipeline ?: 'default';
-
-		return call_user_func(
-			$this->pipelines[$pipeline], new Pipeline($this->container), $object
-		);
-	}
-
+        return call_user_func(
+            $this->pipelines[$pipeline], new Pipeline($this->container), $object
+        );
+    }
 }
--- vendor/royalcms/framework/Royalcms/Component/Pipeline/PipelineServiceProvider.php
+++ vendor/royalcms/framework/Royalcms/Component/Pipeline/PipelineServiceProvider.php
@@ -1,41 +1,39 @@
-<?php 
+<?php
 
 namespace Royalcms\Component\Pipeline;
 
 use Royalcms\Component\Support\ServiceProvider;
 
-class PipelineServiceProvider extends ServiceProvider 
+class PipelineServiceProvider extends ServiceProvider
 {
+    /**
+     * Indicates if loading of the provider is deferred.
+     *
+     * @var bool
+     */
+    protected $defer = true;
 
-	/**
-	 * Indicates if loading of the provider is deferred.
-	 *
-	 * @var bool
-	 */
-	protected $defer = true;
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->royalcms->singleton(
+            'Royalcms\Component\Contracts\Pipeline\Hub', 'Royalcms\Component\Pipeline\Hub'
+        );
+    }
 
-	/**
-	 * Register the service provider.
-	 *
-	 * @return void
-	 */
-	public function register()
-	{
-		$this->royalcms->singleton(
-			'Royalcms\Component\Pipeline\Contracts\Hub', 'Royalcms\Component\Pipeline\Hub'
-		);
-	}
-
-	/**
-	 * Get the services provided by the provider.
-	 *
-	 * @return array
-	 */
-	public function provides()
-	{
-		return [
-			'Royalcms\Component\Pipeline\Contracts\Hub',
-		];
-	}
-
+    /**
+     * Get the services provided by the provider.
+     *
+     * @return array
+     */
+    public function provides()
+    {
+        return [
+            'Royalcms\Component\Contracts\Pipeline\Hub',
+        ];
+    }
 }
