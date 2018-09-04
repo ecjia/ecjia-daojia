47d70fff1b0cd651d5ec13148767fee1c7192b20
王正东 <hxroyal@gmail.com>
2018-09-04 15:45:30 +0800

    更新命名空间

--- vendor/composer/autoload_files.php
+++ vendor/composer/autoload_files.php
@@ -13,4 +13,11 @@ return array(
     $vendorDir . '/guzzlehttp/guzzle/src/functions_include.php',
     $vendorDir . '/guzzlehttp/promises/src/functions_include.php',
     $vendorDir . '/ezyang/htmlpurifier/library/HTMLPurifier.composer.php',
+    $vendorDir . '/symfony/polyfill-mbstring/bootstrap.php',
+    $vendorDir . '/hamcrest/hamcrest-php/hamcrest/Hamcrest.php',
+    $vendorDir . '/symfony/var-dumper/Resources/functions/dump.php',
+    $vendorDir . '/symfony/polyfill-php56/bootstrap.php',
+    $vendorDir . '/paragonie/random_compat/lib/random.php',
+    $vendorDir . '/danielstjules/stringy/src/Create.php',
+
 );
