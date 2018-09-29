ced75ad2d7527e08f6c49aab2ded4aa1ecbf763d
王正东 <hxroyal@gmail.com>
2018-09-29 10:49:17 +0800

    更新友情链接应用模块

--- content/apps/friendlink/configs/package.php
+++ content/apps/friendlink/configs/package.php
@@ -14,6 +14,7 @@ return array(
 	'version' 		=> '1.18.0',						/* 版本号 */
 	'copyright' 	=> 'ECJIA Copyright 2014 ~ 2018.',
     'namespace'     => 'Ecjia\App\Friendlink',
+    'provider'      => 'FriendlinkServiceProvider',
 );
 
 // end
