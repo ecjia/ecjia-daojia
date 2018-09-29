975ea8daf6cb0ce3cd5b6d3155b148011bacd44a
王正东 <hxroyal@gmail.com>
2018-09-29 10:55:16 +0800

    更新开店向导应用模块

--- content/apps/shopguide/configs/package.php
+++ content/apps/shopguide/configs/package.php
@@ -50,15 +50,16 @@ defined('IN_ECJIA') or exit('No permission resources.');
  * 开店向导
  */
 return array(
-    'identifier'  => 'ecjia.shopguide',
-    'directory'   => 'shopguide',
-    'name'        => 'shopguide',
-    'description' => 'shopguide_desc', 			/* 描述对应的语言项 */
-    'author'	  => 'ECJIA TEAM', 				/* 作者 */
-    'website' 	  => 'http://www.ecjia.com', 	/* 网址 */
-    'version' 	  => '1.20.0', 					/* 版本号 */
-    'copyright'   => 'ECJIA Copyright 2016 ~ 2018.',
-    'namespace'   => 'Ecjia\App\Shopguide',
+    'identifier'    => 'ecjia.shopguide',
+    'directory'     => 'shopguide',
+    'name'          => 'shopguide',
+    'description'   => 'shopguide_desc', 			/* 描述对应的语言项 */
+    'author'	    => 'ECJIA TEAM', 				/* 作者 */
+    'website' 	    => 'http://www.ecjia.com', 	/* 网址 */
+    'version' 	    => '1.20.0', 					/* 版本号 */
+    'copyright'     => 'ECJIA Copyright 2016 ~ 2018.',
+    'namespace'     => 'Ecjia\App\Shopguide',
+    'provider'      => 'ShopguideServiceProvider',
 );
 
 // end
