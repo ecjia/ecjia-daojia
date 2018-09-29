21cc8f2fb1b3abb192fc34679f76e56f7a72adbc
王正东 <hxroyal@gmail.com>
2018-09-29 10:47:28 +0800

    更新结算应用模块

--- content/apps/commission/configs/package.php
+++ content/apps/commission/configs/package.php
@@ -59,6 +59,7 @@ return array(
 	'version' 		=> '1.20.0',					/* 版本号 */
 	'copyright' 	=> 'ECJIA Copyright 2016 ~ 2018.',
     'namespace'     => 'Ecjia\App\Commission',
+    'provider'      => 'CommissionServiceProvider',
 );
 
 // end
