8b1d3ed89d8618fb222cc0138140611afbcc6f40
王正东 <hxroyal@gmail.com>
2018-09-29 10:49:03 +0800

    更新商家入驻应用模块

--- content/apps/franchisee/configs/package.php
+++ content/apps/franchisee/configs/package.php
@@ -56,6 +56,7 @@ return array(
 	'version' 		=> '1.19.0',						/* 版本号 */
 	'copyright' 	=> 'ECJIA Copyright 2016 ~ 2018.',
     'namespace'     => 'Ecjia\App\Franchisee',
+    'provider'      => 'FranchiseeServiceProvider',
 );
 
 // end
