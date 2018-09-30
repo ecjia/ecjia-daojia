8325963f26f1b9462a799117e23d52b7d711786b
王正东 <hxroyal@gmail.com>
2018-09-30 13:54:40 +0800

    更新royalcms框架版本为v5.1.0，发布日期为2018-09-30

--- vendor/royalcms/framework/Royalcms/Component/Foundation/Royalcms.php
+++ vendor/royalcms/framework/Royalcms/Component/Foundation/Royalcms.php
@@ -26,14 +26,14 @@ class Royalcms extends Container implements RoyalcmsContract, HttpKernelInterfac
      *
      * @var string
      */
-    const VERSION = '5.0.0';
+    const VERSION = '5.1.0';
 
     /**
      * The Royalcms framework release.
      *
      * @var string
      */
-    const RELEASE = '2018-09-04';
+    const RELEASE = '2018-09-30';
 
     /**
      * The base path for the Laravel installation.
