bb514bd96b2fe71b10cd10752b25831f851e62a5
王正东 <wangzhengdong@ecmoban.com>
2018-11-15 16:08:04 +0800

    更新消息推送应用模块

--- content/apps/push/templates/admin/push_events_list.dwt.php
+++ content/apps/push/templates/admin/push_events_list.dwt.php
@@ -14,6 +14,8 @@
 		<!-- {if $action_link} -->
 		<a class="data-pjax btn plus_or_reply" id="sticky_a" href="{$action_link.href}"><i class="fontello-icon-reply"></i>{$action_link.text}</a>
 		<!-- {/if} -->
+		<a class="all_close plus_or_reply" data-msg="您确定要关闭全部推送消息事件吗？" data-href='{url path="push/admin_events/all_close"}'><button class="btn" type="button">全部关闭</button></a>     
+	    <a class="all_open plus_or_reply"  data-msg="您确定要开启全部推送消息事件吗？" data-href='{url path="push/admin_events/all_open"}'><button class="btn btn-gebo" type="button">全部开启</button></a>     
 	</h3>
 </div>
 
