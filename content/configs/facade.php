9d315e49cbaa2123c02e51d6bb1d58dfcec99246
王正东 <wangzhengdong@ecmoban.com>
2018-11-15 15:56:20 +0800

    更新别名配置

--- content/configs/facade.php
+++ content/configs/facade.php
@@ -2,7 +2,7 @@
 
 return array(
 	
-    'RC_Pay' => 'Royalcms\Component\Omnipay\Facades\Omnipay',
+    'RC_Pay' => 'Royalcms\Component\Pay\Facades\Pay',
     'RC_Redis' => 'Royalcms\Component\Redis\Facades\Redis',
     'RC_WeChat' => 'Royalcms\Component\WeChat\Facades\WeChat',
 
