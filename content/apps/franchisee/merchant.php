88dbe3ee45ead2bf69e6136b5aed0d72f4c3d36a
王正东 <wangzhengdong@ecmoban.com>
2018-11-15 16:02:07 +0800

    更新商家入驻应用模块

--- content/apps/franchisee/merchant.php
+++ content/apps/franchisee/merchant.php
@@ -68,7 +68,7 @@ class merchant extends ecjia_merchant
 
         RC_Script::enqueue_script('migrate', RC_App::apps_url('statics/js/migrate.js', __FILE__), array(), false, true);
         RC_Script::enqueue_script('region', RC_Uri::admin_url('statics/lib/ecjia-js/ecjia.region.js'));
-        RC_Script::enqueue_script('qq_map', 'https://map.qq.com/api/js?v=2.exp');
+        RC_Script::enqueue_script('qq_map', ecjia_location_mapjs());
 
         RC_Loader::load_app_func('merchant_franchisee');
         Ecjia\App\Franchisee\Helper::assign_adminlog_content();
