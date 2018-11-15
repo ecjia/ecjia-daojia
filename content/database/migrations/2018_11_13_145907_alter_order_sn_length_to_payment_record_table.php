c690fdf988559b02f410c3f587b998c02174e1bf
王正东 <wangzhengdong@ecmoban.com>
2018-11-15 15:53:53 +0800

    更新数据表payment_record

---
+++ content/database/migrations/2018_11_13_145907_alter_order_sn_length_to_payment_record_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Royalcms\Component\Database\Schema\Blueprint;
+use Royalcms\Component\Database\Migrations\Migration;
+
+class AlterOrderSnLengthToPaymentRecordTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        RC_Schema::table('payment_record', function (Blueprint $table) {
+            $table->string('order_sn', 60)->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        RC_Schema::table('payment_record', function (Blueprint $table) {
+            //
+        });
+    }
+}
