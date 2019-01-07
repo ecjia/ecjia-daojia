83c47d23d349f70de1174a0fbd88f14dfab3d90a
王正东 <wangzhengdong@ecmoban.com>
2019-01-07 15:15:18 +0800

    添加新的pay站点

---
+++ sites/pay/index.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * 网站指定入口
+ */
+
+define('RC_SITE', 'pay');
+
+define('USE_SUB_DOMAIN', false);
+
+include_once (dirname(__FILE__) . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'index.php');
+
+// end
