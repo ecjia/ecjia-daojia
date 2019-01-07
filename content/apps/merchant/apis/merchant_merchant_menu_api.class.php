dc020afbfeb8a01f61fceeb8d30b4d54da863fd8
王正东 <wangzhengdong@ecmoban.com>
2019-01-07 15:16:34 +0800

    更新商家应用

--- content/apps/merchant/apis/merchant_merchant_menu_api.class.php
+++ content/apps/merchant/apis/merchant_merchant_menu_api.class.php
@@ -50,27 +50,33 @@ defined('IN_ECJIA') or exit('No permission resources.');
  * 商家店铺管理菜单
  * @author songqian
  */
-class merchant_merchant_menu_api extends Component_Event_Api {
+class merchant_merchant_menu_api extends Component_Event_Api
+{
 
-    public function call(&$options) {
+    public function call(&$options)
+    {
         $menus = ecjia_merchant::make_admin_menu('staff', '仪表盘', RC_Uri::url('merchant/dashboard/init'), 0)->add_icon('fa-dashboard')->add_base('dashboard');
 
-        $mymenus = ecjia_merchant::make_admin_menu('14_merchant', __('店铺'), '', 10)->add_icon('fa-home')->add_base('store')->add_purview(array('merchant_manage', 'franchisee_manage', 'bank_manage', 'shopguide_manage'));
+        $mymenus  = ecjia_merchant::make_admin_menu('14_merchant', __('店铺'), '', 10)->add_icon('fa-home')->add_base('store')->add_purview(array('merchant_manage', 'franchisee_manage', 'bank_manage', 'shopguide_manage'));
         $submenus = array(
-            ecjia_merchant::make_admin_menu('01_merchant_setinfo', __('店铺设置'), RC_Uri::url('merchant/merchant/init'), 1)->add_purview('merchant_manage')->add_icon('fa-gears'), //'merchant_info'
-            ecjia_merchant::make_admin_menu('02_merchant_franchisee', __('入驻信息'), RC_Uri::url('merchant/mh_franchisee/init'), 2)->add_purview('franchisee_manage')->add_icon('fa-info-circle'), //'enter_info'
-        	ecjia_merchant::make_admin_menu('03_merchant_Receipt', __('收款账号'), RC_Uri::url('merchant/mh_franchisee/receipt'), 3)->add_purview('franchisee_bank')->add_icon('fa-credit-card'), //'enter_info'
+            ecjia_merchant::make_admin_menu('01_merchant_setinfo', __('店铺设置'), RC_Uri::url('merchant/merchant/init'), 1)->add_purview('merchant_manage')->add_icon('fa-gears'),
+            ecjia_merchant::make_admin_menu('02_merchant_franchisee', __('入驻信息'), RC_Uri::url('merchant/mh_franchisee/init'), 2)->add_purview('franchisee_manage')->add_icon('fa-info-circle'),
+            ecjia_merchant::make_admin_menu('03_merchant_Receipt', __('收款账号'), RC_Uri::url('merchant/mh_franchisee/receipt'), 3)->add_purview('franchisee_bank')->add_icon('fa-credit-card'),
             ecjia_merchant::make_admin_menu('04_merchant_switch', __('店铺打烊'), RC_Uri::url('merchant/merchant/mh_switch'), 4)->add_purview('merchant_switch')->add_icon('fa-power-off'),
             ecjia_merchant::make_admin_menu('05_merchant_template', __('小程序模板'), RC_Uri::url('merchant/merchant/template'), 5)->add_purview('merchant_template')->add_icon('fa-navicon'),
-            ecjia_merchant::make_admin_menu('06_merchant_template', __('PC店铺首页模板'), RC_Uri::url('merchant/merchant/store_template'), 6)->add_purview('merchant_template')->add_icon('fa-navicon'),
         );
-        
+
         $mymenus->add_submenu($submenus);
+
+        if (ecjia::config('template') == 'dscmall') {
+            $mymenus->add_submenu(ecjia_merchant::make_admin_menu('06_merchant_template', __('PC店铺首页模板'), RC_Uri::url('merchant/merchant/store_template'), 6)->add_purview('merchant_template')->add_icon('fa-navicon'));
+        };
+
         $mymenus = RC_Hook::apply_filters('merchant_merchant_menu_api', $mymenus);
         $mymenus = RC_Hook::apply_filters('cashier_merchant_merchant_menu_api', $mymenus);
-        
+
         if ($mymenus->has_submenus()) {
-        	return array($menus, $mymenus);
+            return array($menus, $mymenus);
         }
         return false;
     }
