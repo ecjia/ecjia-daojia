9d30c4053ae7cde6bc8589d8fd02b9c75d0f67d5
王正东 <wangzhengdong@ecmoban.com>
2019-01-24 10:45:40 +0800

    更新升级器应用模块

--- content/apps/upgrade/classes/hooks/admin_upgrade.class.php
+++ content/apps/upgrade/classes/hooks/admin_upgrade.class.php
@@ -78,11 +78,22 @@ class admin_upgrade_hooks {
             ecjia_front::$controller->redirect(RC_Uri::url('upgrade/index/init'));
         }
     }
-    
+
+
+    public static function display_upgrade_local_version_checked()
+    {
+        if (version_compare(ecjia::config('ecjia_version'), RC_Config::get('release.version'), '<')) {
+            $upgrade_url = RC_Uri::url('upgrade/index/init');
+            $warning = sprintf(__('您当前已经覆盖了最新版的v%s程序，建议您立即升级数据库结构，升级前做好备份，前往<a href="%s">升级中心</a>。'), RC_Config::get('release.version'), $upgrade_url);
+            ecjia_screen::get_current_screen()->add_admin_notice(new admin_notice($warning));
+        }
+    }
     
 }
 
 RC_Hook::add_action( 'ecjia_admin_finish_launching', array('admin_upgrade_hooks', 'fixed_version_number'), 1 );
 RC_Hook::add_action( 'ecjia_admin_finish_launching', array('admin_upgrade_hooks', 'upgrade_database'), 11 );
+
+RC_Hook::add_action( 'ecjia_admin_dashboard_index', array('admin_upgrade_hooks', 'display_upgrade_local_version_checked') );
 
 // end
