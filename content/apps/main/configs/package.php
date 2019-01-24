f66ddb4cb1dc24df1d162ff6a3dc53ae657b5702
王正东 <wangzhengdong@ecmoban.com>
2019-01-24 11:24:02 +0800

    更新所有更新过应用的版本号至1.27.0

--- content/apps/main/configs/package.php
+++ content/apps/main/configs/package.php
@@ -56,7 +56,7 @@ return array(
 	'description' => 'main_desc',				/* 描述对应的语言项 */
 	'author'      => 'ECJIA TEAM',				/* 作者 */
 	'website'     => 'http://www.ecjia.com',	/* 网址 */
-	'version'     => '2.21.0',					/* 版本号 */
+	'version'     => '2.27.0',					/* 版本号 */
 	'copyright'   => 'ECJIA Copyright 2014 ~ 2019.',
     'namespace'   => 'Ecjia\App\Main',
     'provider'    => 'MainServiceProvider',
