dd90de60a2c81aabe5e1db11d551055455337160
王正东 <wangzhengdong@ecmoban.com>
2019-01-24 10:46:30 +0800

    创建数据表agent_user

---
+++ content/database/migrations/2019_01_23_152223_create_agent_user_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Royalcms\Component\Database\Schema\Blueprint;
+use Royalcms\Component\Database\Migrations\Migration;
+
+class CreateAgentUserTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        RC_Schema::create('agent_user', function (Blueprint $table) {
+            $table->integer('user_id');
+            $table->string('rank_code', 20)->comment('代理等级code');
+            $table->string('province', 20)->comment('省');
+            $table->string('city', 20)->comment('市');
+            $table->string('district', 20)->comment('区');
+
+            $table->primary('user_id', 'user_id');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        RC_Schema::drop('agent_user');
+    }
+}
