fd28f48c9c141341275c444153a7772abc1b6eb9
王正东 <wangzhengdong@ecmoban.com>
2019-01-31 13:56:44 +0800

    更新royalcms/ide-helper

---
+++ vendor/royalcms/ide-helper/Royalcms.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: royalwang
+ * Date: 2019/1/28
+ * Time: 10:10
+ */
+
+/**
+ * Class Royalcms
+ * @see \Royalcms\Component\Foundation\Royalcms
+ */
+class Royalcms
+{
+
+
+
+}
---
+++ vendor/royalcms/ide-helper/aliases/RC_Uploader.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: royalwang
+ * Date: 2019/1/29
+ * Time: 11:30
+ */
+
+class RC_Uploader
+{
+
+}
