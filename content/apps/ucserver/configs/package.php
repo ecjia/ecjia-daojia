8a9e51b2c345c309ae2caf9620b5c7cdf6157f4f
王正东 <wangzhengdong@ecmoban.com>
2019-02-21 15:41:53 +0800

    更新UC服务端应用模块

--- content/apps/ucserver/configs/package.php
+++ content/apps/ucserver/configs/package.php
@@ -52,11 +52,11 @@ defined('IN_ECJIA') or exit('No permission resources.');
 return array(
     'identifier'    => 'ecjia.ucserver',
     'directory'     => 'ucserver',
-    'name'          => 'ucserver',
-    'description'   => 'ucserver_desc', 			/* 描述对应的语言项 */
+    'name'          => __('UC服务端', 'ucserver'),
+    'description'   => __('UCenter是各个产品之间信息直接传递的一个桥梁，通过UcServer无缝整合系列产品，实现用户的一站式注册、登录、退出以及社区其他数据的交互。', 'ucserver'),
     'author' 	    => 'ECJIA TEAM', 				/* 作者 */
     'website' 	    => 'http://www.ecjia.com', 	/* 网址 */
-    'version' 	    => '1.25.0', 					/* 版本号 */
+    'version' 	    => '1.27.4', 					/* 版本号 */
     'copyright'     => 'ECJIA Copyright 2014 ~ 2019.',
     'namespace'     => 'Ecjia\App\Ucserver',
     'provider'      => 'UcserverServiceProvider',
