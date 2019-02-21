e9a2bc76297e3084512374ce82c13d191876c4a4
王正东 <wangzhengdong@ecmoban.com>
2019-02-21 15:41:28 +0800

    更新物流跟踪应用模块

--- content/apps/track/configs/package.php
+++ content/apps/track/configs/package.php
@@ -6,11 +6,11 @@ defined('IN_ECJIA') or exit('No permission resources.');
 return array(
 	'identifier' 	=> 'ecjia.track',
 	'directory' 	=> 'track',
-	'name'			=> 'track',
-	'description' 	=> 'track_desc',			/* 描述对应的语言项 */
+	'name'			=> __('物流跟踪', 'track'),
+	'description' 	=> __('订单物流跟踪管理。', 'track'),
 	'author' 		=> 'ECJIA TEAM',			/* 作者 */
 	'website' 		=> 'http://www.ecjia.com',	/* 网址 */
-	'version' 		=> '1.25.0',					/* 版本号 */
+	'version' 		=> '1.27.4',					/* 版本号 */
 	'copyright' 	=> 'ECJIA Copyright 2014 ~ 2019.',
     'namespace'     => 'Ecjia\App\Track',
     'provider'      => 'TrackServiceProvider',
