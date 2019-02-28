fc7fb29e9437a17dd5b82879bfc3181d2a1b2d94
王正东 <wangzhengdong@ecmoban.com>
2019-02-28 17:03:39 +0800

    更新会员应用模块

--- content/apps/user/modules/admin/user/account/pay_module.class.php
+++ content/apps/user/modules/admin/user/account/pay_module.class.php
@@ -81,6 +81,8 @@ class admin_user_account_pay_module extends api_admin implements api_interface
                 return $result;
             } else {
                 $order['payment'] = $result;
+                $order['payment']['pay_fee'] = $payment_info['pay_fee'] > 0 ? $payment_info['pay_fee'] : 0;
+                $order['payment']['formatted_pay_fee'] = ecjia_price_format($payment_info['pay_fee'], false);
             }
 
             return array('payment' => $order['payment']);
