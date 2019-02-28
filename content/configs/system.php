ce2a604c85e1de7d24d9e09c9388bb2bbc99b227
王正东 <wangzhengdong@ecmoban.com>
2019-02-28 15:28:07 +0800

    更新system配置文件

--- content/configs/system.php
+++ content/configs/system.php
@@ -51,42 +51,42 @@ return array(
      ******************************** 基本配置 ********************************
      */
     //网站时区，Etc/GMT-8 实际表示的是 GMT+8 timezone
-    'timezone' 						=> env('TIMEZONE', 'Etc/GMT-8'), 		
+    'timezone' 						=> env('TIMEZONE', 'Etc/GMT-8'),
     //网站语言包
-    'locale' 						=> 'zh_CN',	
+    'locale' 						=> 'zh_CN',
     //是否Gzip压缩后输出
-    'gzip' 							=> 0, 					
+    'gzip' 							=> 0,
     //密钥
     'auth_key' 						=> env('AUTH_KEY', 'UbGuq4G8uqk9yRKHhiRnUbGuq4G8uqk9'),
     'cipher'                        => 'AES-256-CBC',
 
     //调试开关
-    'debug'                         => env('DEBUG', false), 
+    'debug'                         => env('DEBUG', false),
 
-	'admin_entrance'				=> 'admincp',
-	
-	'admin_enable'					=> true,
+    'admin_entrance'				=> 'admincp',
+
+    'admin_enable'					=> true,
 
     /*
      ********************************URL路由*******************************
      */
     //URL重写模式
-    'url_rewrite'					=> false,		
+    'url_rewrite'					=> false,
     //URL模式（normal：普通模式 pathinfo：PATHINFO模式 cli：命令行模式）
-    'url_mode'						=> 'normal',			
-		
-	
+    'url_mode'						=> 'normal',
+
+
     /*
      ******************************** 模板参数 *******************************
      */
     //
     'tpl_force_specify'             => false,
     //风格
-    'tpl_style'                     => 'default',  
+    'tpl_style'                     => 'default',
     // 信息提示模板
     'tpl_message'                   => 'showmessage.dwt.php',
-    
-	'tpl_usedfront'					=> false,
+
+    'tpl_usedfront'					=> false,
 
 );
 
