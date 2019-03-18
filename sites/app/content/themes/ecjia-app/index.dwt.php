ea507ae386c34abc81b0cbc97849ac341ecf2f73
王正东 <wangzhengdong@ecmoban.com>
2019-03-18 10:27:48 +0800

    更新ecjia-app主题

--- sites/app/content/themes/ecjia-app/index.dwt.php
+++ sites/app/content/themes/ecjia-app/index.dwt.php
@@ -22,20 +22,20 @@
             <div class="ecjia-fr">
                 <ul class="nav hover-font">
                     <li>
-                        <a class="nopjax" href="{$main_url}">首页</a>
+                        <a class="nopjax" href="{$main_url}">{t domain="ecjia-app"}首页{/t}</a>
                     </li>
                     <li {if $active eq 'category'}class="active" {/if}>
-                        <a class="nopjax" href="{$main_goods_url}">商家</a>
+                        <a class="nopjax" href="{$main_goods_url}">{t domain="ecjia-app"}商家{/t}</a>
                     </li>
                     <li class="active">
-                        <a href="javascript:;">下载APP</a>
+                        <a href="javascript:;">{t domain="ecjia-app"}下载APP{/t}</a>
                     </li>
-                    {if $member_url}<li><a class="nopjax" href="{$member_url}" target="_blank">会员中心</a></li>{/if}
+                    {if $member_url}<li><a class="nopjax" href="{$member_url}" target="_blank">{t domain="ecjia-app"}会员中心{/t}</a></li>{/if}
                     {if ecjia::config('merchant_join_close') eq 0}
-                    <li><a class="nopjax" href="{$merchant_url}" target="_blank">商家入驻</a></li>
+                    <li><a class="nopjax" href="{$merchant_url}" target="_blank">{t domain="ecjia-app"}商家入驻{/t}</a></li>
                     {/if}
                     <li>
-                        <a class="nopjax ecjia-back-green" href="{$merchant_login}" target="_blank">商家登录</a>
+                        <a class="nopjax ecjia-back-green" href="{$merchant_login}" target="_blank">{t domain="ecjia-app"}商家登录{/t}</a>
                     </li>
                 </ul>
             </div>
@@ -59,14 +59,14 @@
                         <div class="img">
                             <img src="{RC_Uri::url('weapp/wxacode/init')}" width="200" height="200" />
                         </div>
-                        <p>扫码体验小程序</p>
+                        <p>{t domain="ecjia-app"}扫码体验小程序{/t}</p>
                     </div>
                     {/if}
                     <div class="qrcode-item {if $has_weapp}last{/if}">
                         <div class="img">
                             <img src="{$touch_qrcode}" width="200" height="200" />
                         </div>
-                        <p>扫码体验H5</p>
+                        <p>{t domain="ecjia-app"}扫码体验H5{/t}</p>
                     </div>
                 </div>
 
@@ -80,9 +80,9 @@
 
         <div class="ecjia-content">
             <div class="ecjia-title">
-                <h1 class="fsize-36">商家和配送员可使用原生APP</h1>
-                <p class="top-title">与商城完美对接的原生APP，下载并安装成功后，商家可通过手机掌柜</p>
-                <p class="bottom-title">随时随地管理店铺、商城配送员可通过配送员APP在线抢单、送单并获得相应的配送费用</p>
+                <h1 class="fsize-36">{t domain="ecjia-app"}商家和配送员可使用原生APP{/t}</h1>
+                <p class="top-title">{t domain="ecjia-app"}与商城完美对接的原生APP，下载并安装成功后，商家可通过手机掌柜{/t}</p>
+                <p class="bottom-title">{t domain="ecjia-app"}随时随地管理店铺、商城配送员可通过配送员APP在线抢单、送单并获得相应的配送费用{/t}</p>
             </div>
 
             <div class="content-item">
@@ -91,13 +91,13 @@
                 </div>
                 <div class="right-side ecjia-fr p_r0">
                     <p class="title">
-                        <span class="num">1</span>掌柜</p>
-                    <p class="notice">与我们商城后台完美同步，下载此APP后，可使用入驻商家账号直接登录，使用手机随时随地高效管理自己的店铺、商品、订单等。</p>
+                        <span class="num">1</span>{t domain="ecjia-app"}掌柜{/t}</p>
+                    <p class="notice">{t domain="ecjia-app"}与我们商城后台完美同步，下载此APP后，可使用入驻商家账号直接登录，使用手机随时随地高效管理自己的店铺、商品、订单等。{/t}</p>
                     <div class="button-item">
                         <a class="ecjia-btn icon-btn green" href="https://itunes.apple.com/cn/app/ec-%E6%8E%8C%E6%9F%9C/id1015857619?mt=8" target="_blank">
-                            <i class="iphone icon"></i>iPhone端下载</a>
+                            <i class="iphone icon"></i>{t domain="ecjia-app"}iPhone端下载{/t}</a>
                         <a class="ecjia-btn icon-btn blue" href="http://a.app.qq.com/o/simple.jsp?pkgname=com.ecjia.shopkeeper" target="_blank">
-                            <i class="android icon"></i>Android端下载</a>
+                            <i class="android icon"></i>{t domain="ecjia-app"}Android端下载{/t}</a>
                     </div>
                 </div>
             </div>
@@ -105,14 +105,14 @@
             <div class="content-item">
                 <div class="right-side ecjia-fl">
                     <p class="title">
-                        <span class="num">2</span>配送员</p>
-                    <p class="notice w91">适用于商城配送员使用，如果您已成为我们的配送员，你可下载安装此APP，获取周边商家的订单，随时随地在线抢单、取货、配送等。</p>
+                        <span class="num">2</span>{t domain="ecjia-app"}配送员{/t}</p>
+                    <p class="notice w91">{t domain="ecjia-app"}适用于商城配送员使用，如果您已成为我们的配送员，你可下载安装此APP，获取周边商家的订单，随时随地在线抢单、取货、配送等。{/t}</p>
                     <div class="button-item">
                         <a class="ecjia-btn icon-btn green" href="https://itunes.apple.com/cn/app/ec-%E9%85%8D%E9%80%81%E5%91%98/id1198119772?mt=8"
                             target="_blank">
-                            <i class="iphone icon"></i>iPhone端下载</a>
+                            <i class="iphone icon"></i>{t domain="ecjia-app"}iPhone端下载{/t}</a>
                         <a class="ecjia-btn icon-btn blue" href="http://a.app.qq.com/o/simple.jsp?pkgname=com.ecjia.express" target="_blank">
-                            <i class="android icon"></i>Android端下载</a>
+                            <i class="android icon"></i>{t domain="ecjia-app"}Android端下载{/t}</a>
                     </div>
                 </div>
                 <div class="left-side ecjia-fr">
@@ -126,13 +126,13 @@
                 </div>
                 <div class="right-side ecjia-fr p_r0">
                     <p class="title">
-                        <span class="num">3</span>云店</p>
-                    <p class="notice">让H5手机网站免费在原生APP内运行，消费者只需下载此APP后，扫描你的店铺二维码，即可在此APP内访问H5商城，满足消费者更好的购物体验。</p>
+                        <span class="num">3</span>{t domain="ecjia-app"}云店{/t}</p>
+                    <p class="notice">{t domain="ecjia-app"}让H5手机网站免费在原生APP内运行，消费者只需下载此APP后，扫描你的店铺二维码，即可在此APP内访问H5商城，满足消费者更好的购物体验。{/t}</p>
                     <div class="button-item">
                         <a class="ecjia-btn icon-btn green" href="https://itunes.apple.com/cn/app/id990510286?mt=8" target="_blank">
-                            <i class="iphone icon"></i>iPhone端下载</a>
+                            <i class="iphone icon"></i>{t domain="ecjia-app"}iPhone端下载{/t}</a>
                         <a class="ecjia-btn icon-btn blue" href="http://a.app.qq.com/o/simple.jsp?pkgname=com.ecjia.street" target="_blank">
-                            <i class="android icon"></i>Android端下载</a>
+                            <i class="android icon"></i>{t domain="ecjia-app"}Android端下载{/t}</a>
                     </div>
                 </div>
             </div>
@@ -140,11 +140,11 @@
             <div class="content-item">
                 <div class="right-side ecjia-fl">
                     <p class="title">
-                        <span class="num">4</span>收银通</p>
-                    <p class="notice w91">专门为零售商户量身打造的一款高效，便捷的收银工具，您可使用此APP快捷地进行开单、收款、验单、退款、会员、统计、小票打印等操作，同时还可使用微信、支付宝、刷卡等多种主流收款方式，为您提供一站式收银、管理、运营的金融解决方案！</p>
+                        <span class="num">4</span>{t domain="ecjia-app"}收银通{/t}</p>
+                    <p class="notice w91">{t domain="ecjia-app"}专门为零售商户量身打造的一款高效，便捷的收银工具，您可使用此APP快捷地进行开单、收款、验单、退款、会员、统计、小票打印等操作，同时还可使用微信、支付宝、刷卡等多种主流收款方式，为您提供一站式收银、管理、运营的金融解决方案！{/t}</p>
                     <div class="button-item">
                         <a class="ecjia-btn icon-btn blue" href="https://shouji.baidu.com/software/25429454.html" target="_blank">
-                            <i class="android icon"></i>Android端下载</a>
+                            <i class="android icon"></i>{t domain="ecjia-app"}Android端下载{/t}</a>
                     </div>
                 </div>
                 <div class="left-side ecjia-fr">
@@ -154,11 +154,11 @@
 
             <div class="teach-item">
                 <div class="teach-item-content">
-                    <div class="title">三步教你如何使用APP</div>
+                    <div class="title">{t domain="ecjia-app"}三步教你如何使用APP{/t}</div>
                     <div class="content">
                         <img class="background" src="{$theme_url}images/casing.png" />
                         <img class="inner" src="{$street_qrcode}" />
-                        <p>扫店铺二维码</p>
+                        <p>{t domain="ecjia-app"}扫店铺二维码{/t}</p>
                         <img class="bottom" src="{$theme_url}images/arrow.png" />
                     </div>
                 </div>
@@ -171,8 +171,8 @@
                             <img class="inner" src="{$theme_url}images/step_1.png" />
                         </div>
                         <div class="right">
-                            <p class="step">第一步</p>
-                            <p>打开已下载的APP</p>
+                            <p class="step">{t domain="ecjia-app"}第一步{/t}</p>
+                            <p>{t domain="ecjia-app"}打开已下载的APP{/t}</p>
                         </div>
                     </div>
                     <div class="step-item-li">
@@ -180,8 +180,8 @@
                             <img class="inner" src="{$theme_url}images/step_2.png" />
                         </div>
                         <div class="right">
-                            <p class="step">第二步</p>
-                            <p>扫描店铺二维码</p>
+                            <p class="step">{t domain="ecjia-app"}第二步{/t}</p>
+                            <p>{t domain="ecjia-app"}扫描店铺二维码{/t}</p>
                         </div>
                     </div>
                     <div class="step-item-li">
@@ -189,21 +189,21 @@
                             <img class="inner" src="{$theme_url}images/step_3.png" />
                         </div>
                         <div class="right">
-                            <p class="step">第三步</p>
-                            <p>输登录账号即可使用</p>
+                            <p class="step">{t domain="ecjia-app"}第三步{/t}</p>
+                            <p>{t domain="ecjia-app"}输登录账号即可使用{/t}</p>
                         </div>
                     </div>
                 </div>
             </div>
 
             <div class="clyimges">
-                <div class="cly-title">界面精彩截图</div>
+                <div class="cly-title">{t domain="ecjia-app"}界面精彩截图{/t}</div>
                 <div class="cly-tab">
                     <div class="cly-tab-content">
-                        <div class="tab-li active" data-tab="shopkeeper">到家掌柜</div>
-                        <div class="tab-li" data-tab="distributor">到家配送员</div>
-                        <div class="tab-li" data-tab="cloudshop">云店</div>
-                        <div class="tab-li" data-tab="syt">收银通</div>
+                        <div class="tab-li active" data-tab="shopkeeper">{t domain="ecjia-app"}到家掌柜{/t}</div>
+                        <div class="tab-li" data-tab="distributor">{t domain="ecjia-app"}到家配送员{/t}</div>
+                        <div class="tab-li" data-tab="cloudshop">{t domain="ecjia-app"}云店{/t}</div>
+                        <div class="tab-li" data-tab="syt">{t domain="ecjia-app"}收银通{/t}</div>
                     </div>
                 </div>
                 <div class="wt-30">
@@ -211,27 +211,27 @@
                         <div class="swiper-wrapper">
                             <div class="swiper-slide">
                                 <img src="{$theme_url}images/shopkeeper/1.png">
-                                <p>掌柜主页</p>
+                                <p>{t domain="ecjia-app"}掌柜主页{/t}</p>
                             </div>
                             <div class="swiper-slide">
                                 <img src="{$theme_url}images/shopkeeper/2.png">
-                                <p>商品列表</p>
+                                <p>{t domain="ecjia-app"}商品列表{/t}</p>
                             </div>
                             <div class="swiper-slide">
                                 <img src="{$theme_url}images/shopkeeper/3.png">
-                                <p>发货列表</p>
+                                <p>{t domain="ecjia-app"}发货列表{/t}</p>
                             </div>
                             <div class="swiper-slide">
                                 <img src="{$theme_url}images/shopkeeper/4.png">
-                                <p>派单列表</p>
+                                <p>{t domain="ecjia-app"}派单列表{/t}</p>
                             </div>
                             <div class="swiper-slide">
                                 <img src="{$theme_url}images/shopkeeper/5.png">
-                                <p>活动列表</p>
+                                <p>{t domain="ecjia-app"}活动列表{/t}</p>
                             </div>
                             <div class="swiper-slide">
                                 <img src="{$theme_url}images/shopkeeper/6.png">
-                                <p>店铺设置</p>
+                                <p>{t domain="ecjia-app"}店铺设置{/t}</p>
                             </div>
                         </div>
                         <div class="swiper-button-next"></div>
@@ -242,23 +242,23 @@
                         <div class="swiper-wrapper">
                             <div class="swiper-slide">
                                 <img src="{$theme_url}images/distributor/1.png">
-                                <p>配送员主页</p>
+                                <p>{t domain="ecjia-app"}配送员主页{/t}</p>
                             </div>
                             <div class="swiper-slide">
                                 <img src="{$theme_url}images/distributor/2.png">
-                                <p>待取货</p>
+                                <p>{t domain="ecjia-app"}待取货{/t}</p>
                             </div>
                             <div class="swiper-slide">
                                 <img src="{$theme_url}images/distributor/3.png">
-                                <p>历史配送</p>
+                                <p>{t domain="ecjia-app"}历史配送{/t}</p>
                             </div>
                             <div class="swiper-slide">
                                 <img src="{$theme_url}images/distributor/4.png">
-                                <p>消息</p>
+                                <p>{t domain="ecjia-app"}消息{/t}</p>
                             </div>
                             <div class="swiper-slide">
                                 <img src="{$theme_url}images/distributor/5.png">
-                                <p>用户中心</p>
+                                <p>{t domain="ecjia-app"}用户中心{/t}</p>
                             </div>
                         </div>
                         <div class="swiper-button-next"></div>
@@ -269,23 +269,23 @@
                         <div class="swiper-wrapper">
                             <div class="swiper-slide">
                                 <img src="{$theme_url}images/cloudshop/1.png">
-                                <p>主页</p>
+                                <p>{t domain="ecjia-app"}主页{/t}</p>
                             </div>
                             <div class="swiper-slide">
                                 <img src="{$theme_url}images/cloudshop/2.png">
-                                <p>店铺首页</p>
+                                <p>{t domain="ecjia-app"}店铺首页{/t}</p>
                             </div>
                             <div class="swiper-slide">
                                 <img src="{$theme_url}images/cloudshop/3.png">
-                                <p>扫码购物</p>
+                                <p>{t domain="ecjia-app"}扫码购物{/t}</p>
                             </div>
                             <div class="swiper-slide">
                                 <img src="{$theme_url}images/cloudshop/4.png">
-                                <p>用户中心</p>
+                                <p>{t domain="ecjia-app"}用户中心{/t}</p>
                             </div>
                             <div class="swiper-slide">
                                 <img src="{$theme_url}images/cloudshop/5.png">
-                                <p>付款码</p>
+                                <p>{t domain="ecjia-app"}付款码{/t}</p>
                             </div>
                         </div>
                         <div class="swiper-button-next"></div>
@@ -296,27 +296,27 @@
                         <div class="swiper-wrapper">
                             <div class="swiper-slide">
                                 <img src="{$theme_url}images/syt/1.png">
-                                <p>店铺首页</p>
+                                <p>{t domain="ecjia-app"}店铺首页{/t}</p>
                             </div>
                             <div class="swiper-slide">
                                 <img src="{$theme_url}images/syt/2.png">
-                                <p>开单</p>
+                                <p>{t domain="ecjia-app"}开单{/t}</p>
                             </div>
                             <div class="swiper-slide">
                                 <img src="{$theme_url}images/syt/3.png">
-                                <p>会员列表</p>
+                                <p>{t domain="ecjia-app"}会员列表{/t}</p>
                             </div>
                             <div class="swiper-slide">
                                 <img src="{$theme_url}images/syt/4.png">
-                                <p>会员详情</p>
+                                <p>{t domain="ecjia-app"}会员详情{/t}</p>
                             </div>
                             <div class="swiper-slide">
                                 <img src="{$theme_url}images/syt/5.png">
-                                <p>收款</p>
+                                <p>{t domain="ecjia-app"}收款{/t}</p>
                             </div>
                             <div class="swiper-slide">
                                 <img src="{$theme_url}images/syt/6.png">
-                                <p>资金流水</p>
+                                <p>{t domain="ecjia-app"}资金流水{/t}</p>
                             </div>
                         </div>
                         <div class="swiper-button-next"></div>
@@ -377,7 +377,7 @@
                 <span class="outlink-qrcode">
                     <div class="wechat-code">
                         <img src="{$shop_wechat_qrcode}">
-                        <span>打开微信扫一扫关注</span>
+                        <span>{t domain="ecjia-app"}打开微信扫一扫关注{/t}</span>
                     </div>
                     <a class="wechat" href="javascript:void(0)"></a>
                 </span>
@@ -391,9 +391,9 @@
                     <!-- {/foreach} -->
                 </p>
             </div>
-            <p>{if $company_name}{$company_name} 版权所有{/if} {if ecjia::config('icp_number')}&nbsp;&nbsp;
+            <p>{if $company_name}{$company_name} {t domain="ecjia-app"}版权所有{/t}{/if} {if ecjia::config('icp_number')}&nbsp;&nbsp;
                 <a href="http://www.miibeian.gov.cn" target="_blank"> {ecjia::config('icp_number')}</a>{/if}&nbsp;&nbsp;{$commoninfo.powered}</p>
-            <p>{if $shop_address}地址：{$shop_address} {/if} {if $service_phone} 咨询热线：{$service_phone}{/if}</p>
+            <p>{if $shop_address}{t domain="ecjia-app"}地址：{/t}{$shop_address} {/if} {if $service_phone} {t domain="ecjia-app"}咨询热线：{/t}{$service_phone}{/if}</p>
         </div>
     </div>
     <div style="display:none">
