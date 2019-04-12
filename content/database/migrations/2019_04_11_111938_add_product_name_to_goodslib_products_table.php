1712dc5ac0f98d5330f330253f0feb0256af4dc3
王正东 <wangzhengdong@ecmoban.com>
2019-04-12 10:46:05 +0800

    数据表goodslib_products修改

---
+++ content/database/migrations/2019_04_11_111938_add_product_name_to_goodslib_products_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Royalcms\Component\Database\Schema\Blueprint;
+use Royalcms\Component\Database\Migrations\Migration;
+
+class AddProductNameToGoodslibProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        RC_Schema::table('goodslib_products', function (Blueprint $table) {
+            $table->string('product_name', 180)->default('0')->comment('货品名称')->after('product_sn');
+            $table->decimal('product_shop_price', 10, 2)->unsigned()->nullable()->comment('货品价格')->after('product_name');
+            $table->string('product_bar_code', 100)->nullable()->comment('货品条形码')->after('product_shop_price');
+            $table->string('product_thumb', 255)->nullable()->comment('货品缩略图')->after('product_bar_code');
+            $table->string('product_img', 255)->nullable()->comment('货品大图')->after('product_thumb');
+            $table->string('product_original_img', 255)->nullable()->comment('货品原图')->after('product_img');
+            $table->text('product_desc')->nullable()->comment('促销限制')->after('product_original_img');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        RC_Schema::table('goodslib_products', function (Blueprint $table) {
+            $table->dropColumn('product_name');
+            $table->dropColumn('product_shop_price');
+            $table->dropColumn('product_bar_code');
+            $table->dropColumn('product_thumb');
+            $table->dropColumn('product_img');
+            $table->dropColumn('product_original_img');
+            $table->dropColumn('product_desc');
+        });
+    }
+}
