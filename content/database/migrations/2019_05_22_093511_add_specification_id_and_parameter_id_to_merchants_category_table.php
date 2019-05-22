fec10690628de4b43657e47ecfc102b386ab0ed6
王正东 <wangzhengdong@ecmoban.com>
2019-05-22 16:16:19 +0800

    merchants_category表新增字段

---
+++ content/database/migrations/2019_05_22_093511_add_specification_id_and_parameter_id_to_merchants_category_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Royalcms\Component\Database\Schema\Blueprint;
+use Royalcms\Component\Database\Migrations\Migration;
+
+class AddSpecificationIdAndParameterIdToMerchantsCategoryTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        RC_Schema::table('merchants_category', function (Blueprint $table) {
+            $table->integer('specification_id')->unsigned()->default('0')->comment('绑定规格模板')->after('is_show');
+            $table->integer('parameter_id')->unsigned()->default('0')->comment('绑定参数模板')->after('specification_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        RC_Schema::table('merchants_category', function (Blueprint $table) {
+            $table->dropColumn('specification_id');
+            $table->dropColumn('parameter_id');
+        });
+    }
+}
