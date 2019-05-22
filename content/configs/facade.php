fa328315eefdaba78bd3c648dde56f555967bfaa
王正东 <wangzhengdong@ecmoban.com>
2019-05-22 16:21:18 +0800

    更新类别名

--- content/configs/facade.php
+++ content/configs/facade.php
@@ -2,8 +2,12 @@
 
 return array(
 	
-    'RC_Pay' => 'Royalcms\Component\Pay\Facades\Pay',
-    'RC_Redis' => 'Royalcms\Component\Redis\Facades\Redis',
-    'RC_WeChat' => 'Royalcms\Component\WeChat\Facades\WeChat',
+    'RC_Pay'                => 'Royalcms\Component\Pay\Facades\Pay',
+    'RC_Redis'              => 'Royalcms\Component\Redis\Facades\Redis',
+    'RC_WeChat'             => 'Royalcms\Component\WeChat\Facades\WeChat',
+
+
+    'RC_Storage'            => 'Royalcms\Component\Storage\Facades\Storage',
+    'RC_Upload'             => 'Royalcms\Component\Upload\Facades\Upload',
 
 );
