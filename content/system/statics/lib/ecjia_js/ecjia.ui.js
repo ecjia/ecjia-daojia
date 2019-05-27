7225d7a51049464d7605712929f70b497f940eb9
王正东 <wangzhengdong@ecmoban.com>
2019-05-27 17:00:11 +0800

    更新ecjia系统应用框架

--- content/system/statics/lib/ecjia_js/ecjia.ui.js
+++ content/system/statics/lib/ecjia_js/ecjia.ui.js
@@ -271,7 +271,25 @@
 					}
 				}
 			});
-		}
+		},
+		//toggleStateNew快速切换状态的方法
+		toggleStateNew : function(option){
+			$.ajax({
+				url: option.url,
+				data: {id : option.id , val : option.val},
+
+				type: option.type,
+				dataType: "json",
+				success: function(data){
+					if(data.state == 'error'){
+						ecjia.admin.showmessage(data);
+					}else{
+						data.content ? option.obj.removeClass('toggleOff').addClass('toggleOn') : option.obj.removeClass('toggleOn').addClass('toggleOff');
+						data.pjaxurl ? ecjia.admin.showmessage(data) : ecjia.admin.showmessage(admin_lang.status_success);
+					}
+				}
+			});
+		},
 	};
 
 	/**
@@ -372,7 +390,7 @@
 			console.log(before);
 //		!$parentobj ? console.log('clone-obj方法未设置data-parent参数。') : ecjia.ui.clone_obj(option);
 		!$parentobj ? console.log(admin_lang.clone) : ecjia.ui.clone_obj(option);
-	})
+	});
 
 	/**
 	 * remove-obj删除节点obj
@@ -384,7 +402,7 @@
 		var $this		= $(this),
 			$parentobj	= $this.parents($this.attr('data-parent'));
 		$parentobj.remove();
-	})
+	});
 
 	/**
      * toggleState触发器
@@ -404,7 +422,26 @@
 		var option  = {obj : $this, url : url, id : id, val : val, type : type};
 		e.preventDefault();
 		ecjia.ui.toggleState(option);
-	})
+	});
+	/**
+	 * toggleStateNew触发器
+	 * url      当前请求地址
+	 * id       当前元素的id号
+	 * val      改变的值
+	 * type     发送方式
+	 *
+	 */
+	$(document).on('click', '[data-trigger="toggleStateNew"]', function(e){
+		var $this   = $(this);
+		var url     = $this.attr('data-url');
+		var id      = $this.attr('data-id');
+		var val     = $this.hasClass('toggleOff') ? 1 : 0;
+		var type    = $this.attr('data-type') ? $this.attr('data-type') : "POST";
+
+		var option  = {obj : $this, url : url, id : id, val : val, type : type};
+		e.preventDefault();
+		ecjia.ui.toggleStateNew(option);
+	});
 
 	/**
 	 * removefile触发器
