a0f561e83023e73fa0f4e75d201f8e1580ff4289
王正东 <wangzhengdong@ecmoban.com>
2019-05-31 15:53:07 +0800

    更新royalcms/purifier组件

--- vendor/royalcms/purifier/Royalcms/Component/Purifier/helpers.php
+++ vendor/royalcms/purifier/Royalcms/Component/Purifier/helpers.php
@@ -1,8 +1,33 @@
 <?php
 
 if (!function_exists('clean')) {
+    /**
+     * @param $dirty
+     * @param null $config
+     * @return mixed
+     */
     function clean($dirty, $config = null)
     {
         return royalcms('purifier')->clean($dirty, $config);
+    }
+}
+
+if ( ! function_exists('remove_xss'))
+{
+    /**
+     * xss过滤函数
+     *
+     * @param string $string
+     * @return string
+     */
+    function remove_xss($string)
+    {
+        if (empty($string)) {
+            return $string;
+        }
+
+        $string = preg_replace('/[\x00-\x08\x0B\x0C\x0E-\x1F\x7F]+/S', '', $string);
+
+        return trim(clean($string, 'input'));
     }
 }
