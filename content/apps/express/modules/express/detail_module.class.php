9405ec8affa8080675f52887c386c03b8305405e
王正东 <wangzhengdong@ecmoban.com>
2019-05-31 15:45:11 +0800

    更新配送应用模块

--- content/apps/express/modules/express/detail_module.class.php
+++ content/apps/express/modules/express/detail_module.class.php
@@ -53,7 +53,7 @@ defined('IN_ECJIA') or exit('No permission resources.');
 class express_detail_module extends api_admin implements api_interface {
     public function handleRequest(\Royalcms\Component\HttpKernel\Request $request) {	
     	$this->authadminSession();
-    	if ($_SESSION['admin_id'] <= 0 && $_SESSION['staff_id'] <= 0) {
+    	if ($_SESSION['staff_id'] <= 0) {
             return new ecjia_error(100, 'Invalid session');
         }
     	$express_id = $this->requestData('express_id');
