adaac4f6235325e39d6c56ca5537da958e9aea3b
王正东 <wangzhengdong@ecmoban.com>
2019-06-12 15:17:53 +0800

    更新系统控制器

--- content/system/classes/Controllers/AboutController.php
+++ content/system/classes/Controllers/AboutController.php
@@ -92,7 +92,7 @@ class AboutController extends ecjia_admin
             '<p>' . __('<a href="https://ecjia.com/wiki/帮助:ECJia智能后台:关于" target="_blank">关于ECJia帮助文档</a>') . '</p>'
         );
 
-        RC_Hook::add_action('admin_about_welcome', array('ecjia_admin', 'display_admin_about_welcome'));
+        RC_Hook::add_action('admin_about_welcome', array('Ecjia\System\BaseController\EcjiaAdminController', 'display_admin_about_welcome'));
 
         return $this->display('about_us.dwt');
     }
@@ -120,7 +120,7 @@ class AboutController extends ecjia_admin
             '<p>' . __('<a href="https://ecjia.com/wiki/帮助:ECJia智能后台:关于" target="_blank">关于ECJia帮助文档</a>') . '</p>'
         );
 
-        RC_Hook::add_action('admin_about_welcome', array('ecjia_admin', 'display_admin_about_welcome'));
+        RC_Hook::add_action('admin_about_welcome', array('Ecjia\System\BaseController\EcjiaAdminController', 'display_admin_about_welcome'));
 
         return $this->display('about_team.dwt');
     }
@@ -146,7 +146,7 @@ class AboutController extends ecjia_admin
             '<p>' . __('<a href="https://ecjia.com/wiki/帮助:ECJia智能后台:关于" target="_blank">关于ECJia帮助文档</a>') . '</p>'
         );
 
-        RC_Hook::add_action('admin_about_welcome', array('ecjia_admin', 'display_admin_about_welcome'));
+        RC_Hook::add_action('admin_about_welcome', array('Ecjia\System\BaseController\EcjiaAdminController', 'display_admin_about_welcome'));
 
         /* 系统信息 */
         $sys_info['os']                 = PHP_OS;
