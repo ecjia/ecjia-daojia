e20f1b40b4e383ab058f43c7f5011a634c0fca45
王正东 <wangzhengdong@ecmoban.com>
2019-06-14 14:05:12 +0800

    更新计算器插件

--- content/plugins/calculator/calculator.class.php
+++ content/plugins/calculator/calculator.class.php
@@ -50,11 +50,9 @@ defined('IN_ECJIA') or exit('No permission resources.');
  * 计算器
  */
 class calculator {
-	public function __construct() {
-		
-	}
-	
-	public function display() {
+
+	public function display()
+    {
 	    echo ecjia_admin::$controller->fetch(ecjia_plugin::get_plugin_template('calculator.lbi.php', 'calculator'));
 	}
 }
