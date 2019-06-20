e30353d8733accf0c7b6f60a537a66e20b95134b
王正东 <wangzhengdong@ecmoban.com>
2019-06-20 17:22:12 +0800

    更新royalcms框架为v5.16.0

--- vendor/royalcms/foundation/Royalcms/Component/Foundation/Royalcms.php
+++ vendor/royalcms/foundation/Royalcms/Component/Foundation/Royalcms.php
@@ -26,14 +26,14 @@ class Royalcms extends Container implements RoyalcmsContract, HttpKernelInterfac
      *
      * @var string
      */
-    const VERSION = '5.15.0';
+    const VERSION = '5.16.0';
 
     /**
      * The Royalcms framework release.
      *
      * @var string
      */
-    const RELEASE = '2019-06-14';
+    const RELEASE = '2019-06-20';
 
     /**
      * The base path for the Laravel installation.
