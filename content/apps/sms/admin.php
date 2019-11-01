a3f4b5f7cda52945e7c23760fdd660265483e190
王正东 <wangzhengdong@ecmoban.com>
2019-11-01 13:33:28 +0800

    更新sms应用模块

--- content/apps/sms/admin.php
+++ content/apps/sms/admin.php
@@ -217,7 +217,7 @@ class admin extends ecjia_admin
     	if (!empty($row)) {
     		foreach ($row AS $key => $val) {
     			$row[$key]['last_send'] = RC_Time::local_date(ecjia::config('time_format'), $val['last_send']);
-    			$row[$key]['channel_name'] = RC_DB::table('notification_channels')->where('channel_code', $val['channel_code'])->pluck('channel_name');
+    			$row[$key]['channel_name'] = RC_DB::table('notification_channels')->where('channel_code', $val['channel_code'])->value('channel_name');
     		}
     	}
     
