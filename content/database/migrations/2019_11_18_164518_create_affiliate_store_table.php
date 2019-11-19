307f0b5c09deb26adc43c3e2c6528ca13354af5b
王正东 <wangzhengdong@ecmoban.com>
2019-11-19 09:49:59 +0800

    提交数据表affiliate_store创建脚本

---
+++ content/database/migrations/2019_11_18_164518_create_affiliate_store_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Royalcms\Component\Database\Schema\Blueprint;
+use Royalcms\Component\Database\Migrations\Migration;
+
+class CreateAffiliateStoreTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        RC_Schema::create('affiliate_store', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id')->unsigned()->default('0');
+            $table->string('agent_name', 60)->comment('代理商名称');
+            $table->integer('agent_parent_id')->unsigned()->default('0')->comment('所属A级代理商的id');
+            $table->decimal('level0', 5, 2)->unsigned()->default('0.00')->comment('A级代理直接推荐获得佣金比');
+            $table->decimal('level1', 5, 2)->unsigned()->default('0.00')->comment('B级代理推荐A级获得佣金比');
+            $table->decimal('level2', 5, 2)->unsigned()->default('0.00')->comment('B级代理推荐获得佣金比');
+            $table->integer('add_time')->unsigned()->default('0')->comment('添加时间');
+
+            $table->unique('user_id', 'user_id');
+            $table->index('agent_parent_id', 'agent_parent_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        RC_Schema::drop('affiliate_store');
+    }
+}
