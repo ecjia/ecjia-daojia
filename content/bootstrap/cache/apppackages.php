2645099d1ec1e42f02fd7935b11cba7ed6e369f7
王正东 <wangzhengdong@ecmoban.com>
2021-01-28 15:07:52 +0800

    生成加载缓存

--- content/bootstrap/cache/apppackages.php
+++ content/bootstrap/cache/apppackages.php
@@ -1,2 +1,72 @@
 <?php return array (
+  'ecjia.api' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Ecjia\\App\\Api\\ApiServiceProvider',
+    ),
+    'aliases' => 
+    array (
+    ),
+    'autoload_psr4' => 
+    array (
+      'Ecjia\\App\\Api\\' => '/content/apps/api/classes/',
+    ),
+  ),
+  'ecjia.cron' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Ecjia\\App\\Cron\\CronServiceProvider',
+    ),
+    'aliases' => 
+    array (
+    ),
+    'autoload_psr4' => 
+    array (
+      'Ecjia\\App\\Cron\\' => '/content/apps/cron/classes/',
+    ),
+  ),
+  'ecjia.logviewer' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Ecjia\\App\\Logviewer\\LogviewerServiceProvider',
+    ),
+    'aliases' => 
+    array (
+    ),
+    'autoload_psr4' => 
+    array (
+      'Ecjia\\App\\Logviewer\\' => '/content/apps/logviewer/classes/',
+    ),
+  ),
+  'ecjia.rpc' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Ecjia\\App\\Rpc\\RpcServiceProvider',
+    ),
+    'aliases' => 
+    array (
+    ),
+    'autoload_psr4' => 
+    array (
+      'Ecjia\\App\\Rpc\\' => '/content/apps/rpc/classes/',
+    ),
+  ),
+  'ecjia.sms' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Ecjia\\App\\Sms\\SmsServiceProvider',
+    ),
+    'aliases' => 
+    array (
+    ),
+    'autoload_psr4' => 
+    array (
+      'Ecjia\\App\\Sms\\' => '/content/apps/sms/classes/',
+    ),
+  ),
 );
